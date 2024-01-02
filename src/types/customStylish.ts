2241824dbf967d4ba54f9e61fb859b9fe726dd83
Carrotzpc <zhang.pc3@gmail.com>
2024-01-02 16:30:43 +0800

    feat: add types

---
+++ src/types/customStylish.ts
@@ -0,0 +1,10 @@
+export interface LobeCustomStylish {
+  blur: string;
+  blurStrong: string;
+  bottomScrollbar: string;
+  gradientAnimation: string;
+  markdown: string;
+  markdownInChat: string;
+  noScrollbar: string;
+  resetLinkColor: string;
+}
---
+++ src/types/index.ts
@@ -0,0 +1,15 @@
+import { type HTMLAttributes } from 'react';
+
+export * from './customStylish';
+export * from './customToken';
+export * from './meta';
+
+export type DivProps = HTMLAttributes<HTMLDivElement>;
+
+export type SvgProps = HTMLAttributes<SVGSVGElement>;
+
+export type ImgProps = HTMLAttributes<HTMLImageElement>;
+
+export type InputProps = HTMLAttributes<HTMLInputElement>;
+
+export type TextAreaProps = HTMLAttributes<HTMLTextAreaElement>;
