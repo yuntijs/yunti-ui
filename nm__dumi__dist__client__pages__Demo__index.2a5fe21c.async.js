358f5c22f41885cc763c0de1e4ceaeaea987c496
Carrotzpc <Carrotzpc@users.noreply.github.com>
2024-04-08 06:48:37 +0000

    deploy: d8c20ee7eaa3f5c6b6bcd00368acaa84a11f3d0b

---
+++ nm__dumi__dist__client__pages__Demo__index.2a5fe21c.async.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_yuntijs_ui=self.webpackChunk_yuntijs_ui||[]).push([[1009],{84606:function(f,n,e){e.r(n),e.d(n,{default:function(){return r}});var t=e(90050),a=e(50959),o=function(){var s=(0,t.UO)(),d=s.id,m=(0,t.WF)(),i=m.demos,c=i[d]||{},u=c.component;return u&&(0,a.createElement)(u)},r=o}}]);
