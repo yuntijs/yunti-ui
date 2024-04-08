c66b6c9203a6e5ae1281c54a0b5a553780db3f32
Jandiasnow <88074479@qq.com>
2024-04-08 13:23:02 +0800

    feat: add Alert component

---
+++ src/Alert/demos/Playground.tsx
@@ -0,0 +1,31 @@
+import { StoryBook, useControls, useCreateStore } from '@lobehub/ui';
+import type { AlertProps } from '@yuntijs/ui';
+import { Alert } from '@yuntijs/ui';
+
+export default () => {
+  const store = useCreateStore();
+  const control: AlertProps | any = useControls(
+    {
+      message: 'YuntiUI',
+      description:
+        'The YuntiUI components are inspired by LobeUI and developed based on Antd components, fully compatible with Antd components, and it is recommended to use antd-style as the default css-in-js styling solution.',
+      showIcon: true,
+      type: {
+        options: ['success', 'info', 'warning', 'error'],
+        value: 'info',
+      },
+      bordered: {
+        options: ['dashed', 'solid', 'none'],
+        value: 'dashed',
+      },
+      closable: false,
+      banner: false,
+    },
+    { store }
+  );
+  return (
+    <StoryBook levaStore={store}>
+      <Alert {...control} />
+    </StoryBook>
+  );
+};
