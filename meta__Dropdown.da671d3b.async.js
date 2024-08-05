0c76e12c4801fe9d25470df9776be39289d0e215
Carrotzpc <Carrotzpc@users.noreply.github.com>
2024-08-05 09:02:36 +0000

    deploy: e55dfab2db5991cfa2b2a0ac0d4ced33e16d4d95

---
+++ meta__Dropdown.da671d3b.async.js
@@ -0,0 +1,154 @@
+"use strict";(self.webpackChunk_yuntijs_ui=self.webpackChunk_yuntijs_ui||[]).push([[9327],{50242:function(s,o,n){n.r(o),n.d(o,{demos:function(){return D}});var l=n(90228),r=n.n(l),d=n(87999),P=n.n(d),u=n(75271),O=n(64210),D={"dropdown-demo-demos":{component:u.memo(u.lazy(function(){return n.e(8568).then(n.bind(n,28741))})),asset:{type:"BLOCK",id:"dropdown-demo-demos",refAtomIds:["Dropdown"],dependencies:{"index.tsx":{type:"FILE",value:n(42220).Z},"@ant-design/icons":{type:"NPM",value:"5.4.0"},"@yuntijs/ui":{type:"NPM",value:"1.0.0-beta.62"}},entry:"index.tsx"},context:{"@ant-design/icons":n(98421),"@yuntijs/ui":n(82426)},renderOpts:{compile:function(){var _=P()(r()().mark(function E(){var e,i=arguments;return r()().wrap(function(t){for(;;)switch(t.prev=t.next){case 0:return t.next=2,n.e(7358).then(n.bind(n,97358));case 2:return t.abrupt("return",(e=t.sent).default.apply(e,i));case 3:case"end":return t.stop()}},E)}));function a(){return _.apply(this,arguments)}return a}()}},"dropdown-demo-dropdownrender":{component:u.memo(u.lazy(function(){return n.e(8568).then(n.bind(n,82891))})),asset:{type:"BLOCK",id:"dropdown-demo-dropdownrender",refAtomIds:["Dropdown"],dependencies:{"index.tsx":{type:"FILE",value:n(48201).Z},"@ant-design/icons":{type:"NPM",value:"5.4.0"},"@yuntijs/ui":{type:"NPM",value:"1.0.0-beta.62"},antd:{type:"NPM",value:"5.19.4"}},entry:"index.tsx"},context:{"@ant-design/icons":n(98421),"@yuntijs/ui":n(82426),antd:n(33275)},renderOpts:{compile:function(){var _=P()(r()().mark(function E(){var e,i=arguments;return r()().wrap(function(t){for(;;)switch(t.prev=t.next){case 0:return t.next=2,n.e(7358).then(n.bind(n,97358));case 2:return t.abrupt("return",(e=t.sent).default.apply(e,i));case 3:case"end":return t.stop()}},E)}));function a(){return _.apply(this,arguments)}return a}()}}}},33275:function(s,o,n){n.r(o),n.d(o,{Affix:function(){return l.Z},Alert:function(){return r.Z},Anchor:function(){return d.Z},App:function(){return P.Z},AutoComplete:function(){return u.Z},Avatar:function(){return O.C},BackTop:function(){return D.Z},Badge:function(){return _.Z},Breadcrumb:function(){return a.Z},Button:function(){return E.ZP},Calendar:function(){return e.Z},Card:function(){return i.Z},Carousel:function(){return M.Z},Cascader:function(){return t.Z},Checkbox:function(){return f.Z},Col:function(){return m.Z},Collapse:function(){return v.Z},ColorPicker:function(){return T.Z},ConfigProvider:function(){return c.ZP},DatePicker:function(){return p.default},Descriptions:function(){return B.Z},Divider:function(){return A.Z},Drawer:function(){return C.Z},Dropdown:function(){return I.Z},Empty:function(){return y.Z},Flex:function(){return R.Z},FloatButton:function(){return L.Z},Form:function(){return K.Z},Grid:function(){return U.ZP},Image:function(){return W.Z},Input:function(){return Z.Z},InputNumber:function(){return g.Z},Layout:function(){return x.Z},List:function(){return h.Z},Mentions:function(){return F.Z},Menu:function(){return j.Z},Modal:function(){return z.Z},Pagination:function(){return $.Z},Popconfirm:function(){return G.Z},Popover:function(){return Q.Z},Progress:function(){return H.Z},QRCode:function(){return J.Z},Radio:function(){return V.ZP},Rate:function(){return X.Z},Result:function(){return Y.ZP},Row:function(){return w.Z},Segmented:function(){return b.Z},Select:function(){return k.default},Skeleton:function(){return q.Z},Slider:function(){return nn.Z},Space:function(){return tn.Z},Spin:function(){return on.Z},Statistic:function(){return rn.Z},Steps:function(){return en.Z},Switch:function(){return un.Z},Table:function(){return _n.Z},Tabs:function(){return an.Z},Tag:function(){return En.Z},TimePicker:function(){return ln.Z},Timeline:function(){return Pn.Z},Tooltip:function(){return dn.Z},Tour:function(){return Dn.Z},Transfer:function(){return Mn.Z},Tree:function(){return On.Z},TreeSelect:function(){return fn.Z},Typography:function(){return mn.Z},Upload:function(){return vn.Z},Watermark:function(){return cn.Z},message:function(){return S.ZP},notification:function(){return N.ZP},theme:function(){return sn.Z},version:function(){return Tn.Z}});var l=n(77314),r=n(32142),d=n(24934),P=n(15558),u=n(52124),O=n(54473),D=n(34738),_=n(28163),a=n(22543),E=n(44295),e=n(14192),i=n(54901),M=n(50658),t=n(34817),f=n(33443),m=n(59823),v=n(76580),T=n(22669),c=n(90479),p=n(86751),B=n(56656),A=n(24230),C=n(85660),I=n(58615),y=n(53202),R=n(71414),L=n(98872),K=n(68191),U=n(1196),W=n(43040),Z=n(5024),g=n(7515),x=n(93424),h=n(8827),F=n(72076),j=n(80649),S=n(53350),z=n(54381),N=n(40097),$=n(16642),G=n(54373),Q=n(46104),H=n(95935),J=n(70940),V=n(19480),X=n(14444),Y=n(25081),w=n(38695),b=n(43869),k=n(66321),q=n(63079),nn=n(99865),tn=n(57179),on=n(40901),rn=n(3029),en=n(8561),un=n(72121),_n=n(6183),an=n(45468),En=n(20449),sn=n(74878),ln=n(82028),Pn=n(93432),dn=n(13399),Dn=n(82121),Mn=n(25951),On=n(96753),fn=n(91272),mn=n(71076),vn=n(99641),Tn=n(63605),cn=n(93793)},65017:function(s,o,n){n.r(o),n.d(o,{texts:function(){return r}});var l=n(64210);const r=[{value:"based on antd ",paraId:0,tocIndex:0},{value:"Dropdown",paraId:0,tocIndex:0},{value:" component.",paraId:0,tocIndex:0},{value:`import { DownOutlined } from '@ant-design/icons';
+import { Avatar, Button, Dropdown, Flex, Typography } from '@yuntijs/ui';
+import type { MenuProps } from '@yuntijs/ui';
+
+const { Text } = Typography;
+
+export default () => {
+  const items: MenuProps['items'] = [
+    {
+      label: (
+        <a href="https://botnow.cn" rel="noopener noreferrer" target="_blank">
+          Botnow
+        </a>
+      ),
+      key: 'botnow',
+    },
+  ];
+
+  return (
+    <Dropdown
+      menu={{ items }}
+      menuExtra={{
+        divider: true,
+        content: (
+          <Text style={{ fontSize: 12 }} type="secondary">
+            \u5DF2\u53D1\u5E03\u5E73\u53F0
+          </Text>
+        ),
+      }}
+    >
+      <Button icon={<DownOutlined />} iconPosition="end">
+        <Flex align="center" gap="small">
+          <Avatar size={16} />
+          <span>Botnow</span>
+        </Flex>
+      </Button>
+    </Dropdown>
+  );
+};
+`,paraId:1,tocIndex:1},{value:"dropdownRender",paraId:2}]},48201:function(s,o){o.Z=`import { DownOutlined, PlusOutlined } from '@ant-design/icons';
+import { Avatar, Button, Dropdown, Flex, Typography } from '@yuntijs/ui';
+import type { MenuProps } from '@yuntijs/ui';
+import { Divider } from 'antd';
+
+const { Text } = Typography;
+
+export default () => {
+  const items: MenuProps['items'] = [
+    {
+      label: (
+        <a href="https://botnow.cn" rel="noopener noreferrer" target="_blank">
+          <Flex align="center" gap="small">
+            <Avatar size={16} />
+            <span>Botnow</span>
+          </Flex>
+        </a>
+      ),
+      key: 'botnow',
+    },
+    {
+      label: (
+        <a href="https://lingwu.cn" rel="noopener noreferrer" target="_blank">
+          <Flex align="center" gap="small">
+            <Avatar size={16} />
+            <span>\u7075\u5C0F\u609F</span>
+          </Flex>
+        </a>
+      ),
+      key: 'lingwu',
+    },
+  ];
+
+  return (
+    <Dropdown
+      dropdownRender={menu => (
+        <Flex vertical>
+          <Text style={{ fontSize: 12, padding: '10px 16px 4px' }} type="secondary">
+            \u5DF2\u53D1\u5E03\u5E73\u53F0
+          </Text>
+          <Divider style={{ margin: 0 }} />
+          {menu}
+          <Divider style={{ margin: 0 }} />
+          <Flex align="center" justify="center" style={{ fontSize: 12, padding: '8px 16px' }}>
+            <Button icon={<PlusOutlined />} type="primary">
+              \u6DFB\u52A0
+            </Button>
+          </Flex>
+        </Flex>
+      )}
+      menu={{ items }}
+    >
+      <Button icon={<DownOutlined />} iconPosition="end">
+        <Flex align="center" gap="small">
+          <Avatar size={16} />
+          <span>Botnow</span>
+        </Flex>
+      </Button>
+    </Dropdown>
+  );
+};
+`},42220:function(s,o){o.Z=`import { DownOutlined } from '@ant-design/icons';
+import { Avatar, Button, Dropdown, Flex, Typography } from '@yuntijs/ui';
+import type { MenuProps } from '@yuntijs/ui';
+
+const { Text } = Typography;
+
+export default () => {
+  const items: MenuProps['items'] = [
+    {
+      label: (
+        <a href="https://botnow.cn" rel="noopener noreferrer" target="_blank">
+          <Flex align="center" gap="small">
+            <Avatar size={16} />
+            <span>Botnow</span>
+          </Flex>
+        </a>
+      ),
+      key: 'botnow',
+    },
+    {
+      label: (
+        <a href="https://lingwu.cn" rel="noopener noreferrer" target="_blank">
+          <Flex align="center" gap="small">
+            <Avatar size={16} />
+            <span>\u7075\u5C0F\u609F</span>
+          </Flex>
+        </a>
+      ),
+      key: 'lingwu',
+    },
+  ];
+
+  return (
+    <Dropdown
+      menu={{ items }}
+      menuExtra={{
+        divider: true,
+        content: (
+          <Text style={{ fontSize: 12 }} type="secondary">
+            \u5DF2\u53D1\u5E03\u5E73\u53F0
+          </Text>
+        ),
+      }}
+    >
+      <Button icon={<DownOutlined />} iconPosition="end">
+        <Flex align="center" gap="small">
+          <Avatar size={16} />
+          <span>Botnow</span>
+        </Flex>
+      </Button>
+    </Dropdown>
+  );
+};
+`}}]);
---
+++ meta__LogViewer.8f37ca6b.async.js
@@ -0,0 +1,67 @@
+(self.webpackChunk_yuntijs_ui=self.webpackChunk_yuntijs_ui||[]).push([[3038],{59168:function(i,t,n){var r={"./ar.json":[18444,8444],"./be.json":[5401,5401],"./cs.json":[24815,4815],"./de.json":[11976,1976],"./en.json":[28443,8443],"./es.json":[74957,4957],"./fa.json":[45263,5263],"./fi.json":[94234,4234],"./fr.json":[96792,6792],"./hi.json":[80489,489],"./it.json":[59688,9688],"./ja.json":[98465,8465],"./ko.json":[7265,7265],"./nl.json":[67781,7781],"./pl.json":[29630,9630],"./pt.json":[15896,5896],"./ru.json":[59787,9787],"./sa.json":[84757,4757],"./tr.json":[22794,2794],"./uk.json":[54429,4429],"./vi.json":[97655,7655],"./zh.json":[50622,622]};function o(u){if(!n.o(r,u))return Promise.resolve().then(function(){var d=new Error("Cannot find module '"+u+"'");throw d.code="MODULE_NOT_FOUND",d});var a=r[u],s=a[0];return n.e(a[1]).then(function(){return n.t(s,19)})}o.keys=function(){return Object.keys(r)},o.id=59168,i.exports=o},10158:function(i,t,n){"use strict";n.r(t),n.d(t,{demos:function(){return p}});var r=n(90228),o=n.n(r),u=n(87999),a=n.n(u),s=n(75271),d=n(23822),p={"logviewer-demo-demos":{component:s.memo(s.lazy(function(){return n.e(3565).then(n.bind(n,5311))})),asset:{type:"BLOCK",id:"logviewer-demo-demos",refAtomIds:["LogViewer"],dependencies:{"index.tsx":{type:"FILE",value:n(14673).Z},"@yuntijs/ui":{type:"NPM",value:"1.0.0-beta.62"}},entry:"index.tsx"},context:{"@yuntijs/ui":n(82426)},renderOpts:{compile:function(){var c=a()(o()().mark(function h(){var l,f=arguments;return o()().wrap(function(e){for(;;)switch(e.prev=e.next){case 0:return e.next=2,n.e(7358).then(n.bind(n,97358));case 2:return e.abrupt("return",(l=e.sent).default.apply(l,f));case 3:case"end":return e.stop()}},h)}));function m(){return c.apply(this,arguments)}return m}()}},"logviewer-demo-playground":{component:s.memo(s.lazy(function(){return n.e(3565).then(n.bind(n,45209))})),asset:{type:"BLOCK",id:"logviewer-demo-playground",refAtomIds:["LogViewer"],dependencies:{"index.tsx":{type:"FILE",value:n(80112).Z},"@lobehub/ui":{type:"NPM",value:"1.147.0"},"@yuntijs/ui":{type:"NPM",value:"1.0.0-beta.62"}},entry:"index.tsx"},context:{"@lobehub/ui":n(59806),"@yuntijs/ui":n(82426)},renderOpts:{compile:function(){var c=a()(o()().mark(function h(){var l,f=arguments;return o()().wrap(function(e){for(;;)switch(e.prev=e.next){case 0:return e.next=2,n.e(7358).then(n.bind(n,97358));case 2:return e.abrupt("return",(l=e.sent).default.apply(l,f));case 3:case"end":return e.stop()}},h)}));function m(){return c.apply(this,arguments)}return m}()}}}},94666:function(i,t,n){"use strict";n.r(t),n.d(t,{texts:function(){return o}});var r=n(23822);const o=[{value:"React component that loads and views remote text in the browser lazily and efficiently. Logs can be loaded from static text, a URL, or a WebSocket and including ANSI highlighting. Based on ",paraId:0},{value:`@melloware/react-logviewer
+`,paraId:0},{value:`import { LogViewer } from '@yuntijs/ui';
+
+const LogViewerDemo = () => {
+  return (
+    <LogViewer
+      enableSearch
+      extraLines={1}
+      height={520}
+      selectableLines
+      url="https://gist.githubusercontent.com/helfi92/96d4444aa0ed46c5f9060a789d316100/raw/ba0d30a9877ea5cc23c7afcd44505dbc2bab1538/typical-live_backing.log"
+    />
+  );
+};
+
+export default ProCardDemo;
+`,paraId:1,tocIndex:0}]},80112:function(i,t){"use strict";t.Z=`import { StoryBook, useControls, useCreateStore } from '@lobehub/ui';
+import { LogViewer, LogViewerProps } from '@yuntijs/ui';
+
+export default () => {
+  const store = useCreateStore();
+  const options: LogViewerProps | any = useControls(
+    {
+      caseInsensitive: true,
+      enableHotKeys: true,
+      enableSearch: true,
+      enableSearchNavigation: true,
+      enableGutters: false,
+      enableLineNumbers: true,
+      enableLinks: false,
+      enableMultilineHighlight: true,
+      extraLines: 1,
+      height: 520,
+      selectableLines: true,
+      text: {
+        rows: true,
+        value: '',
+      },
+      url: 'https://gist.githubusercontent.com/helfi92/96d4444aa0ed46c5f9060a789d316100/raw/ba0d30a9877ea5cc23c7afcd44505dbc2bab1538/typical-live_backing.log',
+      refreshInterval: 0,
+    },
+    { store }
+  );
+
+  return (
+    <StoryBook levaStore={store}>
+      <LogViewer {...options} />
+    </StoryBook>
+  );
+};
+`},14673:function(i,t){"use strict";t.Z=`import { LogViewer } from '@yuntijs/ui';
+
+export default () => {
+  return (
+    <LogViewer
+      caseInsensitive
+      enableHotKeys
+      enableSearch
+      extraLines={1}
+      height={520}
+      onLineContentClick={function noRefCheck() {}}
+      selectableLines
+      url="https://gist.githubusercontent.com/helfi92/96d4444aa0ed46c5f9060a789d316100/raw/ba0d30a9877ea5cc23c7afcd44505dbc2bab1538/typical-live_backing.log"
+    />
+  );
+};
+`}}]);
