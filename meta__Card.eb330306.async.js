5cbf9cd445462fe27d7dfcb077572cd1067105de
Carrotzpc <Carrotzpc@users.noreply.github.com>
2024-08-05 09:15:53 +0000

    deploy: 85cc4a400f7d17f4ee911a7311b86472986b4e28

---
+++ meta__Card.eb330306.async.js
@@ -0,0 +1,164 @@
+(self.webpackChunk_yuntijs_ui=self.webpackChunk_yuntijs_ui||[]).push([[9477],{59168:function(l,e,n){var c={"./ar.json":[18444,8444],"./be.json":[5401,5401],"./cs.json":[24815,4815],"./de.json":[11976,1976],"./en.json":[28443,8443],"./es.json":[74957,4957],"./fa.json":[45263,5263],"./fi.json":[94234,4234],"./fr.json":[96792,6792],"./hi.json":[80489,489],"./it.json":[59688,9688],"./ja.json":[98465,8465],"./ko.json":[7265,7265],"./nl.json":[67781,7781],"./pl.json":[29630,9630],"./pt.json":[15896,5896],"./ru.json":[59787,9787],"./sa.json":[84757,4757],"./tr.json":[22794,2794],"./uk.json":[54429,4429],"./vi.json":[97655,7655],"./zh.json":[50622,622]};function o(p){if(!n.o(c,p))return Promise.resolve().then(function(){var f=new Error("Cannot find module '"+p+"'");throw f.code="MODULE_NOT_FOUND",f});var m=c[p],r=m[0];return n.e(m[1]).then(function(){return n.t(r,19)})}o.keys=function(){return Object.keys(c)},o.id=59168,l.exports=o},86442:function(l,e,n){"use strict";n.r(e),n.d(e,{demos:function(){return h}});var c=n(90228),o=n.n(c),p=n(87999),m=n.n(p),r=n(75271),f=n(43054),h={"card-demo-demos":{component:r.memo(r.lazy(function(){return n.e(1058).then(n.bind(n,17291))})),asset:{type:"BLOCK",id:"card-demo-demos",refAtomIds:["Card"],dependencies:{"index.tsx":{type:"FILE",value:n(47966).Z},"@ant-design/icons":{type:"NPM",value:"5.4.0"},"@yuntijs/ui":{type:"NPM",value:"1.0.0-beta.63"}},entry:"index.tsx"},context:{"@ant-design/icons":n(98421),"@yuntijs/ui":n(82426)},renderOpts:{compile:function(){var a=m()(o()().mark(function d(){var s,u=arguments;return o()().wrap(function(t){for(;;)switch(t.prev=t.next){case 0:return t.next=2,n.e(7358).then(n.bind(n,97358));case 2:return t.abrupt("return",(s=t.sent).default.apply(s,u));case 3:case"end":return t.stop()}},d)}));function i(){return a.apply(this,arguments)}return i}()}},"card-demo-meta":{component:r.memo(r.lazy(function(){return n.e(1058).then(n.bind(n,17530))})),asset:{type:"BLOCK",id:"card-demo-meta",refAtomIds:["Card"],dependencies:{"index.tsx":{type:"FILE",value:n(57194).Z},"@yuntijs/ui":{type:"NPM",value:"1.0.0-beta.63"}},entry:"index.tsx"},context:{"@yuntijs/ui":n(82426)},renderOpts:{compile:function(){var a=m()(o()().mark(function d(){var s,u=arguments;return o()().wrap(function(t){for(;;)switch(t.prev=t.next){case 0:return t.next=2,n.e(7358).then(n.bind(n,97358));case 2:return t.abrupt("return",(s=t.sent).default.apply(s,u));case 3:case"end":return t.stop()}},d)}));function i(){return a.apply(this,arguments)}return i}()}},"card-demo-grid":{component:r.memo(r.lazy(function(){return n.e(1058).then(n.bind(n,27426))})),asset:{type:"BLOCK",id:"card-demo-grid",refAtomIds:["Card"],dependencies:{"index.tsx":{type:"FILE",value:n(56201).Z},"@yuntijs/ui":{type:"NPM",value:"1.0.0-beta.63"}},entry:"index.tsx"},context:{"@yuntijs/ui":n(82426)},renderOpts:{compile:function(){var a=m()(o()().mark(function d(){var s,u=arguments;return o()().wrap(function(t){for(;;)switch(t.prev=t.next){case 0:return t.next=2,n.e(7358).then(n.bind(n,97358));case 2:return t.abrupt("return",(s=t.sent).default.apply(s,u));case 3:case"end":return t.stop()}},d)}));function i(){return a.apply(this,arguments)}return i}()}},"card-demo-tab":{component:r.memo(r.lazy(function(){return n.e(1058).then(n.bind(n,90335))})),asset:{type:"BLOCK",id:"card-demo-tab",refAtomIds:["Card"],dependencies:{"index.tsx":{type:"FILE",value:n(44111).Z},"@yuntijs/ui":{type:"NPM",value:"1.0.0-beta.63"},react:{type:"NPM",value:"18.3.1"}},entry:"index.tsx"},context:{"@yuntijs/ui":n(82426),react:n(75271)},renderOpts:{compile:function(){var a=m()(o()().mark(function d(){var s,u=arguments;return o()().wrap(function(t){for(;;)switch(t.prev=t.next){case 0:return t.next=2,n.e(7358).then(n.bind(n,97358));case 2:return t.abrupt("return",(s=t.sent).default.apply(s,u));case 3:case"end":return t.stop()}},d)}));function i(){return a.apply(this,arguments)}return i}()}},"card-demo-playground":{component:r.memo(r.lazy(function(){return n.e(1058).then(n.bind(n,90771))})),asset:{type:"BLOCK",id:"card-demo-playground",refAtomIds:["Card"],dependencies:{"index.tsx":{type:"FILE",value:n(21476).Z},"@lobehub/ui":{type:"NPM",value:"1.147.0"},"@yuntijs/ui":{type:"NPM",value:"1.0.0-beta.63"}},entry:"index.tsx"},context:{"@lobehub/ui":n(59806),"@yuntijs/ui":n(82426)},renderOpts:{compile:function(){var a=m()(o()().mark(function d(){var s,u=arguments;return o()().wrap(function(t){for(;;)switch(t.prev=t.next){case 0:return t.next=2,n.e(7358).then(n.bind(n,97358));case 2:return t.abrupt("return",(s=t.sent).default.apply(s,u));case 3:case"end":return t.stop()}},d)}));function i(){return a.apply(this,arguments)}return i}()}}}},90405:function(l,e,n){"use strict";n.r(e),n.d(e,{texts:function(){return o}});var c=n(43054);const o=[{value:"based on antd ",paraId:0,tocIndex:0},{value:"Card",paraId:0,tocIndex:0},{value:" component.",paraId:0,tocIndex:0},{value:`import { Card } from '@yuntijs/ui';
+
+export default () => {
+  return (
+    <Card
+      title= 'YuntiUI Card'
+      bordered={true}
+      loading={false}
+      hoverable={true}
+    >
+    The YuntiUI components are inspired by LobeUI and developed based on Antd components, fully compatible with Antd components, and it is recommended to use antd-style as the default css-in-js styling solution.
+    </Children>
+  );
+};
+`,paraId:1,tocIndex:1}]},56201:function(l,e){"use strict";e.Z=`import { Card } from '@yuntijs/ui';
+
+export default () => {
+  const content =
+    'The YuntiUI components are inspired by LobeUI and developed based on Antd components, fully compatible with Antd components, and it is recommended to use antd-style as the default css-in-js styling solution. ';
+  const gridStyle: React.CSSProperties = {
+    width: '50%',
+    textAlign: 'center',
+  };
+  return (
+    <Card title="YuntiUI Card.Grid">
+      <Card.Grid hoverable={false} style={gridStyle}>
+        {content}
+      </Card.Grid>
+      <Card.Grid style={gridStyle}>{content}</Card.Grid>
+      <Card.Grid style={gridStyle}>{content}</Card.Grid>
+    </Card>
+  );
+};
+`},57194:function(l,e){"use strict";e.Z=`import { Card, Logo } from '@yuntijs/ui';
+
+export default () => {
+  const description =
+    'The YuntiUI components are inspired by LobeUI and developed based on Antd components, fully compatible with Antd components, and it is recommended to use antd-style as the default css-in-js styling solution. ';
+
+  return (
+    <Card>
+      <Card.Meta avatar={<Logo />} description={description} title="YuntiUI Card.Meta" />
+    </Card>
+  );
+};
+`},21476:function(l,e){"use strict";e.Z=`import { StoryBook, useControls, useCreateStore } from '@lobehub/ui';
+import type { CardProps } from '@yuntijs/ui';
+import { Card } from '@yuntijs/ui';
+
+export default () => {
+  const store = useCreateStore();
+  const control: CardProps | any = useControls(
+    {
+      title: 'YuntiUI Card',
+      extra: 'extra',
+      bordered: true,
+      loading: false,
+      hoverable: true,
+      children:
+        'The YuntiUI components are inspired by LobeUI and developed based on Antd components, fully compatible with Antd components, and it is recommended to use antd-style as the default css-in-js styling solution.',
+      type: {
+        options: ['inner'],
+        value: 'default',
+      },
+      size: {
+        options: ['default', 'small'],
+        value: 'default',
+      },
+    },
+    { store }
+  );
+  return (
+    <StoryBook levaStore={store}>
+      <Card {...control} />
+    </StoryBook>
+  );
+};
+`},44111:function(l,e){"use strict";e.Z=`import { Card } from '@yuntijs/ui';
+import { useState } from 'react';
+
+export default () => {
+  const tabList = [
+    {
+      key: 'Themeable',
+      label: 'Themeable',
+    },
+    {
+      key: 'Fast',
+      label: 'Fast',
+    },
+    {
+      key: 'Light & Dark UI',
+      label: 'Light & Dark UI',
+    },
+  ];
+  const [activeTabKey, setActiveTabKey] = useState<string>(tabList[0].key);
+  const onTabChange = (key: string) => {
+    setActiveTabKey(key);
+  };
+  const contentList: Record<string, React.ReactNode> = {
+    'Themeable': (
+      <p>
+        Provides a simple way to customize default themes, you can change the colors, fonts,
+        breakpoints and everything you need.
+      </p>
+    ),
+    'Fast': (
+      <p>
+        voids unnecessary styles props at runtime, making it more performant than other UI
+        libraries.
+      </p>
+    ),
+    'Light & Dark UI': (
+      <p>
+        Automatic dark mode recognition, NextUI automatically changes the theme when detects HTML
+        theme prop changes.
+      </p>
+    ),
+  };
+  return (
+    <Card
+      activeTabKey={activeTabKey}
+      onTabChange={onTabChange}
+      style={{ width: '100%' }}
+      tabBarExtraContent={<a href="#">More</a>}
+      tabList={tabList}
+      tabProps={{
+        size: 'middle',
+      }}
+    >
+      {contentList[activeTabKey]}
+    </Card>
+  );
+};
+`},47966:function(l,e){"use strict";e.Z=`import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
+import { Card } from '@yuntijs/ui';
+
+export default () => {
+  return (
+    <Card
+      actions={[
+        <SettingOutlined key="setting" />,
+        <EditOutlined key="edit" />,
+        <EllipsisOutlined key="ellipsis" />,
+      ]}
+      bordered={true}
+      cover={
+        <img
+          alt="example"
+          src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+        />
+      }
+      extra="extra"
+      hoverable={true}
+      loading={false}
+      title="YuntiUI Card"
+    >
+      The YuntiUI components are inspired by LobeUI and developed based on Antd components, fully
+      compatible with Antd components, and it is recommended to use antd-style as the default
+      css-in-js styling solution.
+    </Card>
+  );
+};
+`}}]);
---
+++ meta__notification.1bf4cb9c.async.js
@@ -0,0 +1,161 @@
+(self.webpackChunk_yuntijs_ui=self.webpackChunk_yuntijs_ui||[]).push([[4554],{59168:function(a,o,n){var i={"./ar.json":[18444,8444],"./be.json":[5401,5401],"./cs.json":[24815,4815],"./de.json":[11976,1976],"./en.json":[28443,8443],"./es.json":[74957,4957],"./fa.json":[45263,5263],"./fi.json":[94234,4234],"./fr.json":[96792,6792],"./hi.json":[80489,489],"./it.json":[59688,9688],"./ja.json":[98465,8465],"./ko.json":[7265,7265],"./nl.json":[67781,7781],"./pl.json":[29630,9630],"./pt.json":[15896,5896],"./ru.json":[59787,9787],"./sa.json":[84757,4757],"./tr.json":[22794,2794],"./uk.json":[54429,4429],"./vi.json":[97655,7655],"./zh.json":[50622,622]};function e(u){if(!n.o(i,u))return Promise.resolve().then(function(){var d=new Error("Cannot find module '"+u+"'");throw d.code="MODULE_NOT_FOUND",d});var r=i[u],s=r[0];return n.e(r[1]).then(function(){return n.t(s,19)})}e.keys=function(){return Object.keys(i)},e.id=59168,a.exports=e},18006:function(a,o,n){"use strict";n.r(o),n.d(o,{demos:function(){return y}});var i=n(90228),e=n.n(i),u=n(87999),r=n.n(u),s=n(75271),d=n(57393),y={"notification-demo-demos":{component:s.memo(s.lazy(function(){return n.e(6173).then(n.bind(n,21102))})),asset:{type:"BLOCK",id:"notification-demo-demos",refAtomIds:["notification"],dependencies:{"index.tsx":{type:"FILE",value:n(82654).Z},"@yuntijs/ui":{type:"NPM",value:"1.0.0-beta.63"}},entry:"index.tsx"},context:{"@yuntijs/ui":n(82426)},renderOpts:{compile:function(){var m=r()(e()().mark(function p(){var l,f=arguments;return e()().wrap(function(t){for(;;)switch(t.prev=t.next){case 0:return t.next=2,n.e(7358).then(n.bind(n,97358));case 2:return t.abrupt("return",(l=t.sent).default.apply(l,f));case 3:case"end":return t.stop()}},p)}));function c(){return m.apply(this,arguments)}return c}()}},"notification-demo-playground":{component:s.memo(s.lazy(function(){return n.e(6173).then(n.bind(n,33947))})),asset:{type:"BLOCK",id:"notification-demo-playground",refAtomIds:["notification"],dependencies:{"index.tsx":{type:"FILE",value:n(78876).Z},"@lobehub/ui":{type:"NPM",value:"1.147.0"},"@yuntijs/ui":{type:"NPM",value:"1.0.0-beta.63"}},entry:"index.tsx"},context:{"@lobehub/ui":n(59806),"@yuntijs/ui":n(82426)},renderOpts:{compile:function(){var m=r()(e()().mark(function p(){var l,f=arguments;return e()().wrap(function(t){for(;;)switch(t.prev=t.next){case 0:return t.next=2,n.e(7358).then(n.bind(n,97358));case 2:return t.abrupt("return",(l=t.sent).default.apply(l,f));case 3:case"end":return t.stop()}},p)}));function c(){return m.apply(this,arguments)}return c}()}}}},43670:function(a,o,n){"use strict";n.r(o),n.d(o,{texts:function(){return e}});var i=n(57393);const e=[{value:"based on antd ",paraId:0,tocIndex:0},{value:"Notification",paraId:0,tocIndex:0},{value:" component.",paraId:0,tocIndex:0},{value:"Note\uFF1A Pages that use notification need to be wrapped by the app component, otherwise the notification style may be problematic",paraId:1,tocIndex:1},{value:`import { App, Button, notification } from '@yuntijs/ui';
+
+export default () => {
+  return (
+    <App>
+      <Button
+        onClick={() => {
+          notification.warnings({
+            message: 'YuntiUI nitification',
+            description: 'Prompt notification message globally.',
+            errors: [
+              {
+                name: 'YuntiUI',
+                message:
+                  'The YuntiUI components are inspired by LobeUI and developed based on Antd components, fully compatible with Antd components, and it is recommended to use antd-style as the default css-in-js styling solution.',
+              },
+            ],
+          });
+        }}
+      >
+        Open
+      </Button>
+    </App>
+  );
+};
+`,paraId:2,tocIndex:1}]},78876:function(a,o){"use strict";o.Z=`import { StoryBook, useControls, useCreateStore } from '@lobehub/ui';
+import { App, Button, NotificationArgsProps, notification } from '@yuntijs/ui';
+
+export default () => {
+  const store = useCreateStore();
+  const control: NotificationArgsProps | any = useControls(
+    {
+      message: 'YuntiUI nitification',
+      description: 'Prompt notification message globally.',
+      duration: 4.5,
+      key: 'notification',
+      placement: {
+        options: ['top', 'topLeft', 'topRight', 'bottom', 'bottomLeft', 'bottomRight'],
+        value: 'topRight',
+      },
+    },
+    { store }
+  );
+  return (
+    <StoryBook levaStore={store}>
+      <App>
+        <Button
+          onClick={() => {
+            notification.warnings({
+              ...control,
+              errors: [
+                {
+                  name: 'YuntiUI',
+                  message:
+                    'The YuntiUI components are inspired by LobeUI and developed based on Antd components, fully compatible with Antd components, and it is recommended to use antd-style as the default css-in-js styling solution.',
+                },
+              ],
+            });
+          }}
+        >
+          Open
+        </Button>
+      </App>
+    </StoryBook>
+  );
+};
+`},82654:function(a,o){"use strict";o.Z=`import { App, Button, Space, notification } from '@yuntijs/ui';
+
+export default () => {
+  return (
+    <App>
+      <Space>
+        <Button
+          onClick={() => {
+            notification.success({
+              message: 'YuntiUI notification success',
+              description: 'Prompt notification message globally.',
+            });
+          }}
+        >
+          success
+        </Button>
+        <Button
+          onClick={() => {
+            notification.info({
+              message: 'YuntiUI notification info',
+              description: 'Prompt notification message globally.',
+            });
+          }}
+        >
+          info
+        </Button>
+        <Button
+          onClick={() => {
+            notification.warning({
+              message: 'YuntiUI notification warning',
+              description: 'Prompt notification message globally.',
+            });
+          }}
+        >
+          warning
+        </Button>
+        <Button
+          onClick={() => {
+            notification.warn({
+              message: 'YuntiUI notification warn',
+              description: 'Prompt notification message globally.',
+            });
+          }}
+        >
+          warn
+        </Button>
+        <Button
+          onClick={() => {
+            notification.error({
+              message: 'YuntiUI notification error',
+              description: 'Prompt notification message globally.',
+            });
+          }}
+        >
+          error
+        </Button>
+        <Button
+          onClick={() => {
+            notification.warnings({
+              message: 'YuntiUI notification warnings',
+              description: 'Prompt notification message globally.',
+              errors: [
+                {
+                  name: 'YuntiUI',
+                  message:
+                    'The YuntiUI components are inspired by LobeUI and developed based on Antd components, fully compatible with Antd components, and it is recommended to use antd-style as the default css-in-js styling solution.',
+                },
+              ],
+            });
+          }}
+        >
+          warnings
+        </Button>
+        <Button
+          onClick={() => {
+            notification.warns({
+              message: 'YuntiUI notification warns',
+              description: 'Prompt notification message globally.',
+              errors: [
+                {
+                  name: 'YuntiUI',
+                  message:
+                    'The YuntiUI components are inspired by LobeUI and developed based on Antd components, fully compatible with Antd components, and it is recommended to use antd-style as the default css-in-js styling solution.',
+                },
+              ],
+            });
+          }}
+        >
+          warns
+        </Button>
+      </Space>
+    </App>
+  );
+};
+`}}]);
