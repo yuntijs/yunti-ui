4dcff531af8d9c506ae8a25f5812c823dbe8aff0
Carrotzpc <Carrotzpc@users.noreply.github.com>
2024-10-11 07:06:20 +0000

    deploy: 50b42d3b70fe360c7df4e5b35ec5c86aadaf79cc

---
+++ meta__FormHelper.72c6d9bd.async.js
@@ -0,0 +1,162 @@
+"use strict";(self.webpackChunk_yuntijs_ui=self.webpackChunk_yuntijs_ui||[]).push([[990],{26305:function(i,e,n){var r;n.r(e),n.d(e,{demos:function(){return D}});var f=n(90228),a=n.n(f),M=n(87999),m=n.n(M),u=n(75271),v=n(83096),o=n(25144),l=n(33275),p=n(64048),P=n(69548),D={"formhelper-demo-formhelperdemo":{component:u.memo(u.lazy(function(){return n.e(495).then(n.bind(n,14024))})),asset:{type:"BLOCK",id:"formhelper-demo-formhelperdemo",refAtomIds:["FormHelper"],dependencies:{"index.tsx":{type:"FILE",value:n(5514).Z},"@yuntijs/ui":{type:"NPM",value:"1.0.0-beta.68"},antd:{type:"NPM",value:"5.19.4"},react:{type:"NPM",value:"18.3.1"}},entry:"index.tsx"},context:{"@yuntijs/ui":o,antd:l,react:r||(r=n.t(u,2))},renderOpts:{compile:function(){var d=m()(a()().mark(function c(){var s,_=arguments;return a()().wrap(function(t){for(;;)switch(t.prev=t.next){case 0:return t.next=2,n.e(7423).then(n.bind(n,7423));case 2:return t.abrupt("return",(s=t.sent).default.apply(s,_));case 3:case"end":return t.stop()}},c)}));function E(){return d.apply(this,arguments)}return E}()}},"formhelper-demo-hocformhelper":{component:u.memo(u.lazy(function(){return n.e(495).then(n.bind(n,58086))})),asset:{type:"BLOCK",id:"formhelper-demo-hocformhelper",refAtomIds:["FormHelper"],dependencies:{"index.tsx":{type:"FILE",value:n(75711).Z},"@yuntijs/ui":{type:"NPM",value:"1.0.0-beta.68"},react:{type:"NPM",value:"18.3.1"},"./components/RenderContainer.tsx":{type:"FILE",value:n(40234).Z},"./components/FormContent.tsx":{type:"FILE",value:n(62352).Z},antd:{type:"NPM",value:"5.19.4"}},entry:"index.tsx"},context:{"@yuntijs/ui":o,react:r||(r=n.t(u,2)),"/home/runner/work/yunti-ui/yunti-ui/src/FormHelper/demos/components/RenderContainer.tsx":p,"/home/runner/work/yunti-ui/yunti-ui/src/FormHelper/demos/components/FormContent.tsx":P,antd:l},renderOpts:{compile:function(){var d=m()(a()().mark(function c(){var s,_=arguments;return a()().wrap(function(t){for(;;)switch(t.prev=t.next){case 0:return t.next=2,n.e(7423).then(n.bind(n,7423));case 2:return t.abrupt("return",(s=t.sent).default.apply(s,_));case 3:case"end":return t.stop()}},c)}));function E(){return d.apply(this,arguments)}return E}()}},"formhelper-demo-refuseautofocus":{component:u.memo(u.lazy(function(){return n.e(495).then(n.bind(n,3489))})),asset:{type:"BLOCK",id:"formhelper-demo-refuseautofocus",refAtomIds:["FormHelper"],dependencies:{"index.tsx":{type:"FILE",value:n(55521).Z},"@yuntijs/ui":{type:"NPM",value:"1.0.0-beta.68"},react:{type:"NPM",value:"18.3.1"},"./components/RenderContainer.tsx":{type:"FILE",value:n(40234).Z},"./components/FormContent.tsx":{type:"FILE",value:n(62352).Z},antd:{type:"NPM",value:"5.19.4"}},entry:"index.tsx"},context:{"@yuntijs/ui":o,react:r||(r=n.t(u,2)),"/home/runner/work/yunti-ui/yunti-ui/src/FormHelper/demos/components/RenderContainer.tsx":p,"/home/runner/work/yunti-ui/yunti-ui/src/FormHelper/demos/components/FormContent.tsx":P,antd:l},renderOpts:{compile:function(){var d=m()(a()().mark(function c(){var s,_=arguments;return a()().wrap(function(t){for(;;)switch(t.prev=t.next){case 0:return t.next=2,n.e(7423).then(n.bind(n,7423));case 2:return t.abrupt("return",(s=t.sent).default.apply(s,_));case 3:case"end":return t.stop()}},c)}));function E(){return d.apply(this,arguments)}return E}()}},"formhelper-demo-iduseautofocus":{component:u.memo(u.lazy(function(){return n.e(495).then(n.bind(n,34077))})),asset:{type:"BLOCK",id:"formhelper-demo-iduseautofocus",refAtomIds:["FormHelper"],dependencies:{"index.tsx":{type:"FILE",value:n(50846).Z},"@yuntijs/ui":{type:"NPM",value:"1.0.0-beta.68"},react:{type:"NPM",value:"18.3.1"},"./components/RenderContainer.tsx":{type:"FILE",value:n(40234).Z},"./components/FormContent.tsx":{type:"FILE",value:n(62352).Z},antd:{type:"NPM",value:"5.19.4"}},entry:"index.tsx"},context:{"@yuntijs/ui":o,react:r||(r=n.t(u,2)),"/home/runner/work/yunti-ui/yunti-ui/src/FormHelper/demos/components/RenderContainer.tsx":p,"/home/runner/work/yunti-ui/yunti-ui/src/FormHelper/demos/components/FormContent.tsx":P,antd:l},renderOpts:{compile:function(){var d=m()(a()().mark(function c(){var s,_=arguments;return a()().wrap(function(t){for(;;)switch(t.prev=t.next){case 0:return t.next=2,n.e(7423).then(n.bind(n,7423));case 2:return t.abrupt("return",(s=t.sent).default.apply(s,_));case 3:case"end":return t.stop()}},c)}));function E(){return d.apply(this,arguments)}return E}()}},"formhelper-demo-domuseautofocus":{component:u.memo(u.lazy(function(){return n.e(495).then(n.bind(n,98607))})),asset:{type:"BLOCK",id:"formhelper-demo-domuseautofocus",refAtomIds:["FormHelper"],dependencies:{"index.tsx":{type:"FILE",value:n(74833).Z},"@yuntijs/ui":{type:"NPM",value:"1.0.0-beta.68"},react:{type:"NPM",value:"18.3.1"},"./components/RenderContainer.tsx":{type:"FILE",value:n(40234).Z},"./components/FormContent.tsx":{type:"FILE",value:n(62352).Z},antd:{type:"NPM",value:"5.19.4"}},entry:"index.tsx"},context:{"@yuntijs/ui":o,react:r||(r=n.t(u,2)),"/home/runner/work/yunti-ui/yunti-ui/src/FormHelper/demos/components/RenderContainer.tsx":p,"/home/runner/work/yunti-ui/yunti-ui/src/FormHelper/demos/components/FormContent.tsx":P,antd:l},renderOpts:{compile:function(){var d=m()(a()().mark(function c(){var s,_=arguments;return a()().wrap(function(t){for(;;)switch(t.prev=t.next){case 0:return t.next=2,n.e(7423).then(n.bind(n,7423));case 2:return t.abrupt("return",(s=t.sent).default.apply(s,_));case 3:case"end":return t.stop()}},c)}));function E(){return d.apply(this,arguments)}return E}()}}}},69548:function(i,e,n){n.r(e);var r=n(68191),f=n(66321),a=n(7515),M=n(19480),m=n(72121),u=n(5024),v=n(75271),o=n(52676),l=function(){return(0,o.jsxs)(r.Z,{id:"form123",initialValues:{select:""},children:[(0,o.jsxs)("div",{children:[(0,o.jsx)("input",{id:"originRadio",type:"radio",value:"111"}),(0,o.jsx)("label",{htmlFor:"originRadio",children:"HTML native radio"})]}),(0,o.jsx)(r.Z.Item,{label:"select",name:"select",children:(0,o.jsx)(f.default,{disabled:!0,placeholder:"hello select",showSearch:!0,children:(0,o.jsx)(f.default.Option,{children:"123"})})}),(0,o.jsx)(r.Z.Item,{label:"switch",name:"inputNumber",children:(0,o.jsx)(a.Z,{})}),(0,o.jsx)(r.Z.Item,{label:"Radio",children:(0,o.jsx)(M.ZP,{children:"123"})}),(0,o.jsx)(r.Z.Item,{label:"switch",name:"enabled",children:(0,o.jsx)(m.Z,{})}),(0,o.jsx)(r.Z.Item,{label:"password",children:(0,o.jsx)(u.Z.Password,{placeholder:"hello"})}),(0,o.jsx)(r.Z.Item,{label:"username",children:(0,o.jsx)(u.Z,{placeholder:"hello"})})]})};e.default=l},64048:function(i,e,n){n.r(e);var r=n(48305),f=n.n(r),a=n(44295),M=n(75271),m=n(52676),u=function(o){var l=(0,M.useState)(!1),p=f()(l,2),P=p[0],D=p[1];return(0,m.jsxs)(m.Fragment,{children:[(0,m.jsx)(a.ZP,{onClick:D.bind("",!P),children:"Click me!"}),(0,m.jsx)("br",{}),(0,m.jsx)("br",{}),P&&o.children]})};e.default=u},33275:function(i,e,n){n.r(e),n.d(e,{Affix:function(){return r.Z},Alert:function(){return f.Z},Anchor:function(){return a.Z},App:function(){return M.Z},AutoComplete:function(){return m.Z},Avatar:function(){return u.C},BackTop:function(){return v.Z},Badge:function(){return o.Z},Breadcrumb:function(){return l.Z},Button:function(){return p.ZP},Calendar:function(){return P.Z},Card:function(){return D.Z},Carousel:function(){return d.Z},Cascader:function(){return E.Z},Checkbox:function(){return c.Z},Col:function(){return s.Z},Collapse:function(){return _.Z},ColorPicker:function(){return O.Z},ConfigProvider:function(){return t.ZP},DatePicker:function(){return I.default},Descriptions:function(){return C.Z},Divider:function(){return R.Z},Drawer:function(){return h.Z},Dropdown:function(){return y.Z},Empty:function(){return F.Z},Flex:function(){return A.Z},FloatButton:function(){return T.Z},Form:function(){return L.Z},Grid:function(){return B.ZP},Image:function(){return W.Z},Input:function(){return U.Z},InputNumber:function(){return K.Z},Layout:function(){return Z.Z},List:function(){return x.Z},Mentions:function(){return g.Z},Menu:function(){return j.Z},Modal:function(){return S.Z},Pagination:function(){return $.Z},Popconfirm:function(){return z.Z},Popover:function(){return G.Z},Progress:function(){return V.Z},QRCode:function(){return Q.Z},Radio:function(){return Y.ZP},Rate:function(){return J.Z},Result:function(){return X.ZP},Row:function(){return b.Z},Segmented:function(){return w.Z},Select:function(){return k.default},Skeleton:function(){return q.Z},Slider:function(){return nn.Z},Space:function(){return tn.Z},Spin:function(){return en.Z},Statistic:function(){return on.Z},Steps:function(){return rn.Z},Switch:function(){return un.Z},Table:function(){return an.Z},Tabs:function(){return sn.Z},Tag:function(){return mn.Z},TimePicker:function(){return dn.Z},Timeline:function(){return En.Z},Tooltip:function(){return cn.Z},Tour:function(){return _n.Z},Transfer:function(){return fn.Z},Tree:function(){return pn.Z},TreeSelect:function(){return Pn.Z},Typography:function(){return Mn.Z},Upload:function(){return On.Z},Watermark:function(){return vn.Z},message:function(){return H.ZP},notification:function(){return N.ZP},theme:function(){return ln.Z},version:function(){return Dn.Z}});var r=n(77314),f=n(32142),a=n(24934),M=n(24730),m=n(52124),u=n(54473),v=n(34738),o=n(28163),l=n(22543),p=n(44295),P=n(14192),D=n(54901),d=n(50658),E=n(34817),c=n(33443),s=n(59823),_=n(76580),O=n(22669),t=n(90479),I=n(86751),C=n(56656),R=n(24230),h=n(85660),y=n(58615),F=n(53202),A=n(71414),T=n(98872),L=n(68191),B=n(1196),W=n(43040),U=n(5024),K=n(7515),Z=n(93424),x=n(8827),g=n(72076),j=n(80649),H=n(53350),S=n(54381),N=n(40097),$=n(16642),z=n(54373),G=n(46104),V=n(95935),Q=n(70940),Y=n(19480),J=n(14444),X=n(25081),b=n(38695),w=n(43869),k=n(66321),q=n(63079),nn=n(99865),tn=n(57179),en=n(40901),on=n(3029),rn=n(8561),un=n(72121),an=n(6183),sn=n(45468),mn=n(20449),ln=n(74878),dn=n(82028),En=n(93432),cn=n(13399),_n=n(82121),fn=n(25951),pn=n(96753),Pn=n(91272),Mn=n(71076),On=n(99641),Dn=n(63605),vn=n(93793)},56843:function(i,e,n){n.r(e),n.d(e,{texts:function(){return f}});var r=n(83096);const f=[{value:"Form enhancement component, currently supporting:",paraId:0,tocIndex:0},{value:"autoFocus: automatically selecting the first non-disabled and non-readonly input or textarea of the form when focused.",paraId:1,tocIndex:0},{value:"Wrap the Form component with FormHelper. Supported attributes include:",paraId:2,tocIndex:2},{value:"props",paraId:3,tocIndex:2},{value:"description",paraId:3,tocIndex:2},{value:"default",paraId:3,tocIndex:2},{value:"required",paraId:3,tocIndex:2},{value:"autoFocus",paraId:3,tocIndex:2},{value:"Automatically select the focus state of the first non-disabled and non-readonly input or textarea in the form.",paraId:3,tocIndex:2},{value:"true",paraId:3,tocIndex:2},{value:"false",paraId:3,tocIndex:2},{value:"id",paraId:3,tocIndex:2},{value:"The id of the outer wrapping div.",paraId:3,tocIndex:2},{value:"-",paraId:3,tocIndex:2},{value:"false",paraId:3,tocIndex:2},{value:"className",paraId:3,tocIndex:2},{value:"The className of the outer wrapping div.",paraId:3,tocIndex:2},{value:"-",paraId:3,tocIndex:2},{value:"false",paraId:3,tocIndex:2},{value:"style",paraId:3,tocIndex:2},{value:"The style of the outer wrapping div.",paraId:3,tocIndex:2},{value:"-",paraId:3,tocIndex:2},{value:"false",paraId:3,tocIndex:2},{value:"As a higher-order component, withFormHelper supports the same parameters as the React component FormHelper.",paraId:4,tocIndex:4},{value:"Supports passing a ref, id, or DOM element as a parameter.",paraId:5,tocIndex:6}]},74833:function(i,e){e.Z=`import { useAutoFocus } from '@yuntijs/ui';
+import React, { useEffect, useState } from 'react';
+
+import FormContent from './components/FormContent';
+import RenderContainer from './components/RenderContainer';
+
+type IDomUseAutoFocus = Record<string, never>;
+
+const FormContentWrapper = () => {
+  const [fomDom, setFormDom] = useState<HTMLElement | null>(null);
+  useEffect(() => {
+    //  <Form id="form123"> in FormContent
+    setFormDom(document.querySelector('#form123') as HTMLElement);
+  }, []);
+  useAutoFocus(fomDom);
+  return <FormContent />;
+};
+
+const DomUseAutoFocus: React.FC<IDomUseAutoFocus> = () => {
+  return (
+    <RenderContainer>
+      <FormContentWrapper />
+    </RenderContainer>
+  );
+};
+export default DomUseAutoFocus;
+`},5514:function(i,e){e.Z=`import { FormHelper } from '@yuntijs/ui';
+import { Button, Form, Input } from 'antd';
+import React, { useState } from 'react';
+
+const Test = () => {
+  const [open, setOpen] = useState(false);
+  return (
+    <>
+      <Button onClick={setOpen.bind('', !open)}>Click me!</Button>
+      {open && (
+        <FormHelper>
+          <Form>
+            <Form.Item>
+              <Input />
+            </Form.Item>
+          </Form>
+        </FormHelper>
+      )}
+    </>
+  );
+};
+export default Test;
+`},75711:function(i,e){e.Z=`import { withFormHelper } from '@yuntijs/ui';
+import React from 'react';
+
+import FormContent from './components/FormContent';
+import RenderContainer from './components/RenderContainer';
+
+const HocFormHelper: React.FC<Record<string, never>> = () => {
+  const FormContentWrapper = withFormHelper()(FormContent);
+  return (
+    <RenderContainer>
+      <FormContentWrapper />
+    </RenderContainer>
+  );
+};
+export default HocFormHelper;
+`},50846:function(i,e){e.Z=`import { useAutoFocus } from '@yuntijs/ui';
+import React from 'react';
+
+import FormContent from './components/FormContent';
+import RenderContainer from './components/RenderContainer';
+
+const FormContentWrapper = () => {
+  // <Form id="form123"> in FormContent
+  useAutoFocus('form123');
+  return <FormContent />;
+};
+
+const IdUseAutoFocus: React.FC<Record<string, never>> = () => {
+  return (
+    <RenderContainer>
+      <FormContentWrapper />
+    </RenderContainer>
+  );
+};
+export default IdUseAutoFocus;
+`},55521:function(i,e){e.Z=`import { useAutoFocus } from '@yuntijs/ui';
+import React, { useRef } from 'react';
+
+import FormContent from './components/FormContent';
+import RenderContainer from './components/RenderContainer';
+
+const FormContentWrapper = () => {
+  const divRef = useRef(null);
+  useAutoFocus(divRef);
+  // \u26A0\uFE0Fwarning The form instance obtained through the ref of the Ant Design's Form component does not take effect. You need to pass the ref of native HTML tags like div or span.
+  return (
+    <div ref={divRef}>
+      <FormContent />
+    </div>
+  );
+};
+
+const RefUseAutoFocus: React.FC<Record<string, never>> = () => {
+  return (
+    <RenderContainer>
+      <FormContentWrapper />
+    </RenderContainer>
+  );
+};
+export default RefUseAutoFocus;
+`},62352:function(i,e){e.Z=`import { Form, Input, InputNumber, Radio, Select, Switch } from 'antd';
+import React from 'react';
+
+const FormContent: React.FC<Record<string, never>> = () => {
+  return (
+    <Form id="form123" initialValues={{ select: '' }}>
+      <div>
+        <input id="originRadio" type="radio" value="111" />
+        <label htmlFor="originRadio">HTML native radio</label>
+      </div>
+      <Form.Item label="select" name={'select'}>
+        <Select disabled placeholder="hello select" showSearch>
+          <Select.Option>123</Select.Option>
+        </Select>
+      </Form.Item>
+      <Form.Item label="switch" name="inputNumber">
+        <InputNumber />
+      </Form.Item>
+      <Form.Item label="Radio">
+        <Radio>123</Radio>
+      </Form.Item>
+      <Form.Item label="switch" name="enabled">
+        <Switch />
+      </Form.Item>
+      <Form.Item label="password">
+        <Input.Password placeholder="hello" />
+      </Form.Item>
+      <Form.Item label="username">
+        <Input placeholder="hello" />
+      </Form.Item>
+    </Form>
+  );
+};
+export default FormContent;
+`},40234:function(i,e){e.Z=`import { Button } from 'antd';
+import React, { useState } from 'react';
+
+interface IRenderContainer {
+  children: React.ReactNode;
+}
+
+const RenderContainer: React.FC<IRenderContainer> = props => {
+  const [open, setOpen] = useState(false);
+  return (
+    <>
+      <Button onClick={setOpen.bind('', !open)}>Click me!</Button>
+      <br />
+      <br />
+      {open && props.children}
+    </>
+  );
+};
+export default RenderContainer;
+`}}]);
