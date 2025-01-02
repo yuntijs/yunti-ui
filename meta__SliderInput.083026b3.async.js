c07167d7047fad9a2b5f7c54ed5cac175c896d96
Carrotzpc <Carrotzpc@users.noreply.github.com>
2025-01-02 09:46:09 +0000

    deploy: 2df57fd84a89c7b1c6b2a860e33a1a9a177f5d33

---
+++ meta__SliderInput.083026b3.async.js
@@ -0,0 +1,80 @@
+(self.webpackChunk_yuntijs_ui=self.webpackChunk_yuntijs_ui||[]).push([[16723],{69143:function(i,r,n){var _={"./ar.json":[18444,18444],"./be.json":[5401,5401],"./cs.json":[24815,24815],"./de.json":[11976,11976],"./en.json":[28443,28443],"./es.json":[74957,74957],"./fa.json":[45263,45263],"./fi.json":[94234,94234],"./fr.json":[96792,96792],"./hi.json":[80489,80489],"./it.json":[59688,59688],"./ja.json":[98465,98465],"./ko.json":[7265,7265],"./nl.json":[67781,67781],"./pl.json":[29630,29630],"./pt.json":[15896,15896],"./ru.json":[59787,59787],"./sa.json":[84757,84757],"./tr.json":[22794,22794],"./uk.json":[54429,54429],"./vi.json":[97655,97655],"./zh.json":[50622,50622]};function E(o){if(!n.o(_,o))return Promise.resolve().then(function(){var u=new Error("Cannot find module '"+o+"'");throw u.code="MODULE_NOT_FOUND",u});var D=_[o],s=D[0];return n.e(D[1]).then(function(){return n.t(s,19)})}E.keys=function(){return Object.keys(_)},E.id=69143,i.exports=E},16662:function(i,r,n){"use strict";var _;n.r(r),n.d(r,{demos:function(){return v}});var E=n(90228),o=n.n(E),D=n(87999),s=n.n(D),u=n(75271),I=n(6163),m=n(94572),M=n(33275),f=n(17724),v={"sliderinput-demo-demos":{component:u.memo(u.lazy(function(){return n.e(91053).then(n.bind(n,49357))})),asset:{type:"BLOCK",id:"sliderinput-demo-demos",refAtomIds:["SliderInput"],dependencies:{"index.tsx":{type:"FILE",value:n(53987).Z},"@yuntijs/ui":{type:"NPM",value:"1.0.0-beta.73"}},entry:"index.tsx"},context:{"@yuntijs/ui":m},renderOpts:{compile:function(){var a=s()(o()().mark(function P(){var e,d=arguments;return o()().wrap(function(t){for(;;)switch(t.prev=t.next){case 0:return t.next=2,n.e(74807).then(n.bind(n,74807));case 2:return t.abrupt("return",(e=t.sent).default.apply(e,d));case 3:case"end":return t.stop()}},P)}));function l(){return a.apply(this,arguments)}return l}()}},"sliderinput-demo-form":{component:u.memo(u.lazy(function(){return n.e(91053).then(n.bind(n,46246))})),asset:{type:"BLOCK",id:"sliderinput-demo-form",refAtomIds:["SliderInput"],dependencies:{"index.tsx":{type:"FILE",value:n(73631).Z},"@yuntijs/ui":{type:"NPM",value:"1.0.0-beta.73"},antd:{type:"NPM",value:"5.19.4"},react:{type:"NPM",value:"18.3.1"}},entry:"index.tsx"},context:{"@yuntijs/ui":m,antd:M,react:_||(_=n.t(u,2))},renderOpts:{compile:function(){var a=s()(o()().mark(function P(){var e,d=arguments;return o()().wrap(function(t){for(;;)switch(t.prev=t.next){case 0:return t.next=2,n.e(74807).then(n.bind(n,74807));case 2:return t.abrupt("return",(e=t.sent).default.apply(e,d));case 3:case"end":return t.stop()}},P)}));function l(){return a.apply(this,arguments)}return l}()}},"sliderinput-demo-playground":{component:u.memo(u.lazy(function(){return n.e(91053).then(n.bind(n,108))})),asset:{type:"BLOCK",id:"sliderinput-demo-playground",refAtomIds:["SliderInput"],dependencies:{"index.tsx":{type:"FILE",value:n(33221).Z},"@lobehub/ui":{type:"NPM",value:"1.153.16"},"@yuntijs/ui":{type:"NPM",value:"1.0.0-beta.73"}},entry:"index.tsx"},context:{"@lobehub/ui":f,"@yuntijs/ui":m},renderOpts:{compile:function(){var a=s()(o()().mark(function P(){var e,d=arguments;return o()().wrap(function(t){for(;;)switch(t.prev=t.next){case 0:return t.next=2,n.e(74807).then(n.bind(n,74807));case 2:return t.abrupt("return",(e=t.sent).default.apply(e,d));case 3:case"end":return t.stop()}},P)}));function l(){return a.apply(this,arguments)}return l}()}}}},33275:function(i,r,n){"use strict";n.r(r),n.d(r,{Affix:function(){return _.Z},Alert:function(){return E.Z},Anchor:function(){return o.Z},App:function(){return D.Z},AutoComplete:function(){return s.Z},Avatar:function(){return u.C},BackTop:function(){return I.Z},Badge:function(){return m.Z},Breadcrumb:function(){return M.Z},Button:function(){return f.ZP},Calendar:function(){return v.Z},Card:function(){return a.Z},Carousel:function(){return l.Z},Cascader:function(){return P.Z},Checkbox:function(){return e.Z},Col:function(){return d.Z},Collapse:function(){return O.Z},ColorPicker:function(){return t.Z},ConfigProvider:function(){return C.ZP},DatePicker:function(){return c.default},Descriptions:function(){return A.Z},Divider:function(){return B.Z},Drawer:function(){return T.Z},Dropdown:function(){return U.Z},Empty:function(){return L.Z},Flex:function(){return R.Z},FloatButton:function(){return K.Z},Form:function(){return W.Z},Grid:function(){return p.ZP},Image:function(){return y.Z},Input:function(){return Z.Z},InputNumber:function(){return h.Z},Layout:function(){return j.Z},List:function(){return S.Z},Mentions:function(){return g.Z},Menu:function(){return x.Z},Modal:function(){return N.Z},Pagination:function(){return V.Z},Popconfirm:function(){return $.Z},Popover:function(){return z.Z},Progress:function(){return J.Z},QRCode:function(){return Q.Z},Radio:function(){return H.ZP},Rate:function(){return X.Z},Result:function(){return Y.ZP},Row:function(){return b.Z},Segmented:function(){return k.Z},Select:function(){return w.default},Skeleton:function(){return q.Z},Slider:function(){return nn.Z},Space:function(){return tn.Z},Spin:function(){return rn.Z},Statistic:function(){return on.Z},Steps:function(){return un.Z},Switch:function(){return en.Z},Table:function(){return _n.Z},Tabs:function(){return En.Z},Tag:function(){return sn.Z},TimePicker:function(){return ln.Z},Timeline:function(){return Pn.Z},Tooltip:function(){return dn.Z},Tour:function(){return Dn.Z},Transfer:function(){return mn.Z},Tree:function(){return On.Z},TreeSelect:function(){return Mn.Z},Typography:function(){return fn.Z},Upload:function(){return vn.Z},Watermark:function(){return Cn.Z},message:function(){return F.ZP},notification:function(){return G.ZP},theme:function(){return an.Z},version:function(){return In.Z}});var _=n(77314),E=n(32142),o=n(24934),D=n(24730),s=n(52124),u=n(54473),I=n(34738),m=n(28163),M=n(22543),f=n(44295),v=n(14192),a=n(54901),l=n(50658),P=n(34817),e=n(33443),d=n(59823),O=n(76580),t=n(22669),C=n(90479),c=n(86751),A=n(56656),B=n(24230),T=n(85660),U=n(58615),L=n(53202),R=n(71414),K=n(98872),W=n(68191),p=n(1196),y=n(43040),Z=n(5024),h=n(7515),j=n(93424),S=n(8827),g=n(72076),x=n(80649),F=n(53350),N=n(54381),G=n(40097),V=n(54887),$=n(25600),z=n(46104),J=n(95935),Q=n(70940),H=n(19480),X=n(14444),Y=n(25081),b=n(38695),k=n(43869),w=n(66321),q=n(63079),nn=n(99865),tn=n(57179),rn=n(40901),on=n(3029),un=n(8561),en=n(72121),_n=n(6183),En=n(45468),sn=n(20449),an=n(74878),ln=n(82028),Pn=n(7448),dn=n(13399),Dn=n(82121),mn=n(25951),On=n(96753),Mn=n(91272),fn=n(71076),vn=n(99641),In=n(63605),Cn=n(93793)},21879:function(i,r,n){"use strict";n.r(r),n.d(r,{texts:function(){return E}});var _=n(6163);const E=[{value:"A Slider and Input component for displaying current value and intervals in range.",paraId:0}]},73631:function(i,r){"use strict";r.Z=`import { SliderInput } from '@yuntijs/ui';
+import { Button, Divider, Form } from 'antd';
+import { useState } from 'react';
+
+const SliderInputFormDemo = () => {
+  const [values, setValues] = useState({});
+  return (
+    <>
+      <Form
+        colon={false}
+        initialValues={{
+          CPU: 4,
+          memory: 16,
+          GPU: 0,
+        }}
+        labelAlign="left"
+        labelCol={{ flex: '110px' }}
+        labelWrap
+        name="slider"
+        onFinish={formValues => setValues(formValues)}
+        wrapperCol={{ flex: 1 }}
+      >
+        <Form.Item label="CPU" name="CPU" rules={[{ required: true }]}>
+          <SliderInput addonAfter="\u6838" max={16} min={0.01} step={0.01} />
+        </Form.Item>
+
+        <Form.Item label="\u5185\u5B58" name="memory" rules={[{ required: true }]}>
+          <SliderInput addonAfter="GB" max={256} min={0.5} step={0.5} />
+        </Form.Item>
+
+        <Form.Item label="GPU" name="GPU" rules={[{ required: true }]}>
+          <SliderInput addonAfter="\u9897" max={10} min={0} step={1} />
+        </Form.Item>
+
+        <Form.Item label=" ">
+          <Button htmlType="submit" type="primary">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+      <Divider orientation="left" plain>
+        Form values
+      </Divider>
+      <pre>{JSON.stringify(values, null, 2)}</pre>
+    </>
+  );
+};
+
+export default SliderInputFormDemo;
+`},33221:function(i,r){"use strict";r.Z=`import { StoryBook, useControls, useCreateStore } from '@lobehub/ui';
+import { SliderInput, SliderInputProps } from '@yuntijs/ui';
+
+export default () => {
+  const store = useCreateStore();
+  const controls: SliderInputProps | any = useControls(
+    {
+      min: 0.1,
+      max: 16,
+      step: 0.01,
+      gutter: 16,
+      defaultValue: 4,
+      addonAfter: '\u6838',
+      addonBefore: '',
+    },
+    { store }
+  );
+  return (
+    <StoryBook levaStore={store}>
+      <SliderInput inputCol={{ span: 7 }} style={{ width: '100%' }} {...controls} />
+    </StoryBook>
+  );
+};
+`},53987:function(i,r){"use strict";r.Z=`import { SliderInput } from '@yuntijs/ui';
+
+const SliderInputDemo = () => {
+  return <SliderInput addonAfter="\u6838" max={16} min={0.01} placeholder="CPU" step={0.01} />;
+};
+
+export default SliderInputDemo;
+`}}]);
