6ae3fecf0d42ab2940fb4df4a31441a3c687dd09
Carrotzpc <Carrotzpc@users.noreply.github.com>
2025-02-10 08:20:45 +0000

    deploy: fbfc3d6ba856f477056a26f5f9bc0087fb499752

--- meta__Form.4cde0fb2.async.js
+++ meta__Form.4cde0fb2.async.js
@@ -1,6 +1,6 @@
-(self.webpackChunk_yuntijs_ui=self.webpackChunk_yuntijs_ui||[]).push([[70252],{72239:function(s,t,n){var e={"./ar.json":[18444,18444],"./be.json":[5401,5401],"./cs.json":[24815,24815],"./de.json":[11976,11976],"./en.json":[28443,28443],"./es.json":[74957,74957],"./fa.json":[45263,45263],"./fi.json":[94234,94234],"./fr.json":[96792,96792],"./hi.json":[80489,80489],"./it.json":[59688,59688],"./ja.json":[98465,98465],"./ko.json":[7265,7265],"./nl.json":[67781,67781],"./pl.json":[29630,29630],"./pt.json":[15896,15896],"./ru.json":[59787,59787],"./sa.json":[84757,84757],"./tr.json":[22794,22794],"./uk.json":[54429,54429],"./vi.json":[97655,97655],"./zh.json":[50622,50622]};function r(a){if(!n.o(e,a))return Promise.resolve().then(function(){var l=new Error("Cannot find module '"+a+"'");throw l.code="MODULE_NOT_FOUND",l});var u=e[a],i=u[0];return n.e(u[1]).then(function(){return n.t(i,19)})}r.keys=function(){return Object.keys(e)},r.id=72239,s.exports=r},82137:function(s,t,n){"use strict";var e;n.r(t),n.d(t,{demos:function(){return C}});var r=n(90228),a=n.n(r),u=n(87999),i=n.n(u),l=n(75271),m=n(71983),y=n(70766),h=n(6747),I=n(53193),x=n(77902),g=n(16911),P=n(45464),C={"src-form-demo-demos":{component:l.memo(l.lazy(function(){return Promise.all([n.e(53541),n.e(74107),n.e(86749),n.e(86147),n.e(3409),n.e(65146),n.e(39270),n.e(52433)]).then(n.bind(n,58809))})),asset:{type:"BLOCK",id:"src-form-demo-demos",refAtomIds:[],dependencies:{"index.tsx":{type:"FILE",value:n(47376).Z},"@yuntijs/ui":{type:"NPM",value:"1.0.0-beta.101"},"lucide-react":{type:"NPM",value:"0.417.0"},react:{type:"NPM",value:"18.3.1"}},entry:"index.tsx"},context:{"@yuntijs/ui":m,"lucide-react":y,react:e||(e=n.t(l,2))},renderOpts:{compile:function(){var c=i()(a()().mark(function v(){var d,f=arguments;return a()().wrap(function(o){for(;;)switch(o.prev=o.next){case 0:return o.next=2,n.e(92074).then(n.bind(n,92074));case 2:return o.abrupt("return",(d=o.sent).default.apply(d,f));case 3:case"end":return o.stop()}},v)}));function p(){return c.apply(this,arguments)}return p}()}},"src-form-demo-tree":{component:l.memo(l.lazy(function(){return Promise.all([n.e(53541),n.e(74107),n.e(86749),n.e(86147),n.e(3409),n.e(65146),n.e(39270),n.e(52433)]).then(n.bind(n,25822))})),asset:{type:"BLOCK",id:"src-form-demo-tree",refAtomIds:[],dependencies:{"index.tsx":{type:"FILE",value:n(60388).Z},"@ant-design/icons":{type:"NPM",value:"5.4.0"},"@yuntijs/ui":{type:"NPM",value:"1.0.0-beta.101"},"lucide-react":{type:"NPM",value:"0.417.0"},react:{type:"NPM",value:"18.3.1"},"./style.ts":{type:"FILE",value:n(23329).Z},"antd-style":{type:"NPM",value:"3.7.1"}},entry:"index.tsx"},context:{"./style.ts":I,"@ant-design/icons":h,"@yuntijs/ui":m,"lucide-react":y,react:e||(e=n.t(l,2)),"/home/runner/work/yunti-ui/yunti-ui/src/Form/demos/style.ts":I,"antd-style":x},renderOpts:{compile:function(){var c=i()(a()().mark(function v(){var d,f=arguments;return a()().wrap(function(o){for(;;)switch(o.prev=o.next){case 0:return o.next=2,n.e(92074).then(n.bind(n,92074));case 2:return o.abrupt("return",(d=o.sent).default.apply(d,f));case 3:case"end":return o.stop()}},v)}));function p(){return c.apply(this,arguments)}return p}()}},"src-form-demo-playground":{component:l.memo(l.lazy(function(){return Promise.all([n.e(53541),n.e(74107),n.e(86749),n.e(86147),n.e(3409),n.e(65146),n.e(39270),n.e(52433)]).then(n.bind(n,59356))})),asset:{type:"BLOCK",id:"src-form-demo-playground",refAtomIds:[],dependencies:{"index.tsx":{type:"FILE",value:n(43315).Z},"@lobehub/ui":{type:"NPM",value:"1.164.2"},"@yuntijs/ui":{type:"NPM",value:"1.0.0-beta.101"},antd:{type:"NPM",value:"5.23.4"},"lucide-react":{type:"NPM",value:"0.417.0"}},entry:"index.tsx"},context:{"@lobehub/ui":g,"@yuntijs/ui":m,antd:P,"lucide-react":y},renderOpts:{compile:function(){var c=i()(a()().mark(function v(){var d,f=arguments;return a()().wrap(function(o){for(;;)switch(o.prev=o.next){case 0:return o.next=2,n.e(92074).then(n.bind(n,92074));case 2:return o.abrupt("return",(d=o.sent).default.apply(d,f));case 3:case"end":return o.stop()}},v)}));function p(){return c.apply(this,arguments)}return p}()}}}},53193:function(s,t,n){"use strict";n.r(t),n.d(t,{useStyles:function(){return i}});var e=n(53649),r=n.n(e),a=n(3772),u,i=(0,a.kc)(function(l){var m=l.css,y=l.token,h=l.prefixCls;return{operation:m(u||(u=r()([`
+(self.webpackChunk_yuntijs_ui=self.webpackChunk_yuntijs_ui||[]).push([[70252],{72239:function(s,t,n){var e={"./ar.json":[18444,18444],"./be.json":[5401,5401],"./cs.json":[24815,24815],"./de.json":[11976,11976],"./en.json":[28443,28443],"./es.json":[74957,74957],"./fa.json":[45263,45263],"./fi.json":[94234,94234],"./fr.json":[96792,96792],"./hi.json":[80489,80489],"./it.json":[59688,59688],"./ja.json":[98465,98465],"./ko.json":[7265,7265],"./nl.json":[67781,67781],"./pl.json":[29630,29630],"./pt.json":[15896,15896],"./ru.json":[59787,59787],"./sa.json":[84757,84757],"./tr.json":[22794,22794],"./uk.json":[54429,54429],"./vi.json":[97655,97655],"./zh.json":[50622,50622]};function r(l){if(!n.o(e,l))return Promise.resolve().then(function(){var a=new Error("Cannot find module '"+l+"'");throw a.code="MODULE_NOT_FOUND",a});var u=e[l],i=u[0];return n.e(u[1]).then(function(){return n.t(i,19)})}r.keys=function(){return Object.keys(e)},r.id=72239,s.exports=r},82137:function(s,t,n){"use strict";var e;n.r(t),n.d(t,{demos:function(){return C}});var r=n(90228),l=n.n(r),u=n(87999),i=n.n(u),a=n(75271),m=n(71983),y=n(70766),h=n(6747),I=n(53193),x=n(77902),g=n(16911),P=n(45464),C={"src-form-demo-demos":{component:a.memo(a.lazy(function(){return Promise.all([n.e(11895),n.e(45169),n.e(69894),n.e(30477),n.e(86147),n.e(3409),n.e(78835),n.e(39270),n.e(52433)]).then(n.bind(n,58809))})),asset:{type:"BLOCK",id:"src-form-demo-demos",refAtomIds:[],dependencies:{"index.tsx":{type:"FILE",value:n(47376).Z},"@yuntijs/ui":{type:"NPM",value:"1.0.0-beta.101"},"lucide-react":{type:"NPM",value:"0.417.0"},react:{type:"NPM",value:"18.3.1"}},entry:"index.tsx"},context:{"@yuntijs/ui":m,"lucide-react":y,react:e||(e=n.t(a,2))},renderOpts:{compile:function(){var c=i()(l()().mark(function v(){var d,f=arguments;return l()().wrap(function(o){for(;;)switch(o.prev=o.next){case 0:return o.next=2,n.e(92074).then(n.bind(n,92074));case 2:return o.abrupt("return",(d=o.sent).default.apply(d,f));case 3:case"end":return o.stop()}},v)}));function p(){return c.apply(this,arguments)}return p}()}},"src-form-demo-tree":{component:a.memo(a.lazy(function(){return Promise.all([n.e(11895),n.e(45169),n.e(69894),n.e(30477),n.e(86147),n.e(3409),n.e(78835),n.e(39270),n.e(52433)]).then(n.bind(n,25822))})),asset:{type:"BLOCK",id:"src-form-demo-tree",refAtomIds:[],dependencies:{"index.tsx":{type:"FILE",value:n(60388).Z},"@ant-design/icons":{type:"NPM",value:"5.4.0"},"@yuntijs/ui":{type:"NPM",value:"1.0.0-beta.101"},"lucide-react":{type:"NPM",value:"0.417.0"},react:{type:"NPM",value:"18.3.1"},"./style.ts":{type:"FILE",value:n(23329).Z},"antd-style":{type:"NPM",value:"3.7.1"}},entry:"index.tsx"},context:{"./style.ts":I,"@ant-design/icons":h,"@yuntijs/ui":m,"lucide-react":y,react:e||(e=n.t(a,2)),"/home/runner/work/yunti-ui/yunti-ui/src/Form/demos/style.ts":I,"antd-style":x},renderOpts:{compile:function(){var c=i()(l()().mark(function v(){var d,f=arguments;return l()().wrap(function(o){for(;;)switch(o.prev=o.next){case 0:return o.next=2,n.e(92074).then(n.bind(n,92074));case 2:return o.abrupt("return",(d=o.sent).default.apply(d,f));case 3:case"end":return o.stop()}},v)}));function p(){return c.apply(this,arguments)}return p}()}},"src-form-demo-playground":{component:a.memo(a.lazy(function(){return Promise.all([n.e(11895),n.e(45169),n.e(69894),n.e(30477),n.e(86147),n.e(3409),n.e(78835),n.e(39270),n.e(52433)]).then(n.bind(n,59356))})),asset:{type:"BLOCK",id:"src-form-demo-playground",refAtomIds:[],dependencies:{"index.tsx":{type:"FILE",value:n(43315).Z},"@lobehub/ui":{type:"NPM",value:"1.164.2"},"@yuntijs/ui":{type:"NPM",value:"1.0.0-beta.101"},antd:{type:"NPM",value:"5.23.4"},"lucide-react":{type:"NPM",value:"0.417.0"}},entry:"index.tsx"},context:{"@lobehub/ui":g,"@yuntijs/ui":m,antd:P,"lucide-react":y},renderOpts:{compile:function(){var c=i()(l()().mark(function v(){var d,f=arguments;return l()().wrap(function(o){for(;;)switch(o.prev=o.next){case 0:return o.next=2,n.e(92074).then(n.bind(n,92074));case 2:return o.abrupt("return",(d=o.sent).default.apply(d,f));case 3:case"end":return o.stop()}},v)}));function p(){return c.apply(this,arguments)}return p}()}}}},53193:function(s,t,n){"use strict";n.r(t),n.d(t,{useStyles:function(){return i}});var e=n(53649),r=n.n(e),l=n(3772),u,i=(0,l.kc)(function(a){var m=a.css,y=a.token,h=a.prefixCls;return{operation:m(u||(u=r()([`
       justify-content: flex-end;
-    `])))}})},77902:function(s,t,n){"use strict";n.r(t),n.d(t,{StyleProvider:function(){return e.V9},ThemeProvider:function(){return e.f6},createGlobalStyle:function(){return e.vJ},createInstance:function(){return e.Fs},createStyles:function(){return e.kc},createStylish:function(){return e.Yz},css:function(){return e.iv},cx:function(){return e.cx},extractStaticStyle:function(){return e.Y2},injectGlobal:function(){return e.hi},keyframes:function(){return e.F4},setupStyled:function(){return e.Us},styleManager:function(){return e.Rr},useAntdStylish:function(){return r.n},useAntdTheme:function(){return a.A},useAntdToken:function(){return u.S},useResponsive:function(){return i.F},useTheme:function(){return e.Fg},useThemeMode:function(){return l.r}});var e=n(3772),r=n(52921),a=n(25185),u=n(29621),i=n(46092),l=n(71795)},99262:function(s,t,n){"use strict";n.r(t),n.d(t,{texts:function(){return e}});const e=[{value:"Based on ",paraId:0,tocIndex:0},{value:"antd form",paraId:0,tocIndex:0},{value:", add ",paraId:0,tocIndex:0},{value:"Form.CollapseList",paraId:0,tocIndex:0},{value:" component.",paraId:0,tocIndex:0},{value:"Form",paraId:1},{value:"see ",paraId:2,tocIndex:1},{value:"https://ant.design/components/form",paraId:2,tocIndex:1},{value:" .",paraId:2,tocIndex:1},{value:"Form.CollapseList",paraId:1},{value:`import { Form, Input } from '@yuntijs/ui';
+    `])))}})},77902:function(s,t,n){"use strict";n.r(t),n.d(t,{StyleProvider:function(){return e.V9},ThemeProvider:function(){return e.f6},createGlobalStyle:function(){return e.vJ},createInstance:function(){return e.Fs},createStyles:function(){return e.kc},createStylish:function(){return e.Yz},css:function(){return e.iv},cx:function(){return e.cx},extractStaticStyle:function(){return e.Y2},injectGlobal:function(){return e.hi},keyframes:function(){return e.F4},setupStyled:function(){return e.Us},styleManager:function(){return e.Rr},useAntdStylish:function(){return r.n},useAntdTheme:function(){return l.A},useAntdToken:function(){return u.S},useResponsive:function(){return i.F},useTheme:function(){return e.Fg},useThemeMode:function(){return a.r}});var e=n(3772),r=n(52921),l=n(25185),u=n(29621),i=n(46092),a=n(71795)},99262:function(s,t,n){"use strict";n.r(t),n.d(t,{texts:function(){return e}});const e=[{value:"Based on ",paraId:0,tocIndex:0},{value:"antd form",paraId:0,tocIndex:0},{value:", add ",paraId:0,tocIndex:0},{value:"Form.CollapseList",paraId:0,tocIndex:0},{value:" component.",paraId:0,tocIndex:0},{value:"Form",paraId:1},{value:"see ",paraId:2,tocIndex:1},{value:"https://ant.design/components/form",paraId:2,tocIndex:1},{value:" .",paraId:2,tocIndex:1},{value:"Form.CollapseList",paraId:1},{value:`import { Form, Input } from '@yuntijs/ui';
 import { VariableIcon } from 'lucide-react';
 
 const FormCollapseList = Form.CollapseList;
@@ -51,7 +51,7 @@ export default () => {
     </Form>
   );
 };
-`,paraId:3,tocIndex:3},{value:"\u652F\u6301\u6811\u5F62\u5C55\u793A\u548C\u8F93\u5165\uFF0C\u53F6\u5B50\u8282\u70B9\u56FA\u5B9A\u5728 ",paraId:4,tocIndex:4},{value:"children",paraId:4,tocIndex:4},{value:" \u5B57\u6BB5\u4E2D\uFF0C\u4E0D\u652F\u6301\u81EA\u5B9A\u4E49\u3002\u5728 column render \u5C5E\u6027\u4E2D\u8FD4\u56DE\u4E86 operation\uFF0C\u4E0E ",paraId:4,tocIndex:4},{value:"Form.List",paraId:4,tocIndex:4},{value:" \u7684 operation \u7528\u6CD5\u4E00\u81F4\u3002",paraId:4,tocIndex:4},{value:"\u53EF\u4EE5\u901A\u8FC7\u8BBE\u7F6E ",paraId:5,tocIndex:4},{value:"indentSize",paraId:5,tocIndex:4},{value:" \u4EE5\u63A7\u5236\u6BCF\u4E00\u5C42\u7684\u7F29\u8FDB\u5BBD\u5EA6\u3002",paraId:5,tocIndex:4},{value:"\u8BBE\u7F6E ",paraId:6,tocIndex:4},{value:"defaultExpandAllRows",paraId:6,tocIndex:4},{value:" \u53EF\u63A7\u5236\u521D\u59CB\u65F6\uFF0C\u662F\u5426\u5C55\u5F00\u6240\u6709\u884C\u3002",paraId:6,tocIndex:4},{value:`<FormCollapseList
+`,paraId:3,tocIndex:3},{value:"\u652F\u6301\u6811\u5F62\u5C55\u793A\u548C\u8F93\u5165\uFF0C\u5F53\u6570\u636E\u4E2D\u6709 ",paraId:4,tocIndex:4},{value:"children",paraId:4,tocIndex:4},{value:" \u5B57\u6BB5\u65F6\u4F1A\u81EA\u52A8\u5C55\u793A\u4E3A\u6811\u5F62\u8868\u683C\uFF0C\uFF0C\u5982\u679C\u4E0D\u9700\u8981\u6216\u914D\u7F6E\u4E3A\u5176\u4ED6\u5B57\u6BB5\u53EF\u4EE5\u7528 ",paraId:4,tocIndex:4},{value:"childrenColumnName",paraId:4,tocIndex:4},{value:" \u8FDB\u884C\u914D\u7F6E\u3002\u5728 column render \u5C5E\u6027\u4E2D\u8FD4\u56DE\u4E86 operation\uFF0C\u4E0E ",paraId:4,tocIndex:4},{value:"Form.List",paraId:4,tocIndex:4},{value:" \u7684 operation \u7528\u6CD5\u4E00\u81F4\u3002",paraId:4,tocIndex:4},{value:"\u53EF\u4EE5\u901A\u8FC7\u8BBE\u7F6E ",paraId:5,tocIndex:4},{value:"indentSize",paraId:5,tocIndex:4},{value:" \u4EE5\u63A7\u5236\u6BCF\u4E00\u5C42\u7684\u7F29\u8FDB\u5BBD\u5EA6\u3002",paraId:5,tocIndex:4},{value:"\u8BBE\u7F6E ",paraId:6,tocIndex:4},{value:"defaultExpandAllRows",paraId:6,tocIndex:4},{value:" \u53EF\u63A7\u5236\u521D\u59CB\u65F6\uFF0C\u662F\u5426\u5C55\u5F00\u6240\u6709\u884C\u3002",paraId:6,tocIndex:4},{value:`<FormCollapseList
   columns={[
     {
       render: (fieldName, index, operation, fieldPath) => {
@@ -149,7 +149,7 @@ interface FieldType {
   outputs?: {
     name: string;
     value: string;
-    children?: FieldType['outputs'];
+    items?: FieldType['outputs'];
   }[];
 }
 
@@ -158,11 +158,11 @@ const initialValues: FieldType = {
     {
       name: 'class',
       value: 'claas1',
-      children: [
+      items: [
         {
           name: 'c1',
           value: 'c1',
-          children: [
+          items: [
             {
               name: 'c11',
               value: 'c11',
@@ -195,18 +195,22 @@ export default () => {
         variant="filled"
       >
         <FormCollapseList
+          childrenColumnName="items"
           columns={[
             {
-              dependencies: fieldPath => [['outputs', ...fieldPath, 'value']],
+              dependencies: parentFieldPath => [['outputs', ...parentFieldPath, 'value']],
               label: '\u53D8\u91CF\u540D',
               name: 'name',
               render: () => <Input maxLength={30} placeholder="\u8F93\u5165\u53D8\u91CF\u540D" showCount />,
-              rules: (fieldPath, index) => [
+              rules: (parentFieldPath, index) => [
                 { whitespace: true, required: true, message: '\u8BF7\u8F93\u5165\u53D8\u91CF\u540D' },
                 ({ getFieldValue }) => ({
                   validator(_rule, value) {
-                    const vars: FieldType['outputs'] = getFieldValue(['outputs', ...fieldPath]);
-                    if (vars?.some((v, vIndex) => vIndex !== index && v.name === value)) {
+                    const vars: FieldType['outputs'] = getFieldValue([
+                      'outputs',
+                      ...parentFieldPath,
+                    ]);
+                    if (vars?.some((v, vIndex) => vIndex !== index && v?.name === value)) {
                       return Promise.reject(new Error('\u53D8\u91CF\u540D\u4E0D\u80FD\u91CD\u590D'));
                     }
                     return Promise.resolve();
@@ -217,19 +221,19 @@ export default () => {
             {
               label: '\u53D8\u91CF\u503C',
               name: 'value',
-              render: () => <Input placeholder="\u8F93\u5165\u53D8\u91CF\u503C" style={{ width: 360 }} />,
+              render: () => <Input placeholder="\u8F93\u5165\u53D8\u91CF\u503C" />,
               rules: [{ required: true, message: '\u8BF7\u8F93\u5165\u53D8\u91CF\u503C' }],
             },
             {
               className: styles.operation,
-              render: (fieldName, index, operation, fieldPath) => {
+              render: (fieldName, _index, operation, fieldPath) => {
                 return (
                   <Space size="small">
                     <Tooltip title="\u6DFB\u52A0\u53F6\u5B50\u8282\u70B9">
                       <Button
                         icon={<PlusOutlined />}
                         onClick={() => {
-                          operation.add();
+                          operation.add({ name: 'key' });
                         }}
                         type="text"
                       />
@@ -243,7 +247,7 @@ export default () => {
                             }
                           }}
                         >
-                          <Form.Item name={['outputs', ...fieldPath, index, 'desc']}>
+                          <Form.Item name={['outputs', ...fieldPath, 'desc']}>
                             <Input.TextArea placeholder="\u8BF7\u8F93\u5165\u63CF\u8FF0" />
                           </Form.Item>
                         </Form>
@@ -254,7 +258,7 @@ export default () => {
                       <Button icon={<FileDoneOutlined />} type="text" />
                     </Popover>
                     <Button
-                      disabled={outputs.length === 1}
+                      disabled={outputs.length === 1 && fieldPath.lengh === 1}
                       icon={<DeleteOutlined />}
                       onClick={() => operation.remove(fieldName)}
                       type="text"
