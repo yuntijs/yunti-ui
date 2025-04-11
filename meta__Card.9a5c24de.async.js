08ec48443243e4a9d43a5a526aa92306a679c728
Carrotzpc <Carrotzpc@users.noreply.github.com>
2025-04-11 03:31:17 +0000

    deploy: d0d924b3029e50bc014c7570c0ecbb6556f93d29

---
+++ meta__Card.9a5c24de.async.js
@@ -0,0 +1,164 @@
+(self.webpackChunk_yuntijs_ui=self.webpackChunk_yuntijs_ui||[]).push([[59477],{72239:function(l,t,n){var m={"./ar.json":[18444,18444],"./be.json":[5401,5401],"./cs.json":[24815,24815],"./de.json":[11976,11976],"./en.json":[28443,28443],"./es.json":[74957,74957],"./fa.json":[45263,45263],"./fi.json":[94234,94234],"./fr.json":[96792,96792],"./hi.json":[80489,80489],"./it.json":[59688,59688],"./ja.json":[98465,98465],"./ko.json":[7265,7265],"./nl.json":[67781,67781],"./pl.json":[29630,29630],"./pt.json":[15896,15896],"./ru.json":[59787,59787],"./sa.json":[84757,84757],"./tr.json":[22794,22794],"./uk.json":[54429,54429],"./vi.json":[97655,97655],"./zh.json":[50622,50622]};function p(o){if(!n.o(m,o))return Promise.resolve().then(function(){var s=new Error("Cannot find module '"+o+"'");throw s.code="MODULE_NOT_FOUND",s});var f=m[o],c=f[0];return n.e(f[1]).then(function(){return n.t(c,19)})}p.keys=function(){return Object.keys(m)},p.id=72239,l.exports=p},61379:function(l,t,n){"use strict";var m;n.r(t),n.d(t,{demos:function(){return g}});var p=n(90228),o=n.n(p),f=n(87999),c=n.n(f),s=n(75271),v=n(6747),y=n(594),C=n(16911),g={"card-demo-demos":{component:s.memo(s.lazy(function(){return n.e(91058).then(n.bind(n,74937))})),asset:{type:"BLOCK",id:"card-demo-demos",refAtomIds:["Card"],dependencies:{"index.tsx":{type:"FILE",value:n(28999).Z},"@ant-design/icons":{type:"NPM",value:"5.4.0"},"@yuntijs/ui":{type:"NPM",value:"1.2.0"}},entry:"index.tsx"},context:{"@ant-design/icons":v,"@yuntijs/ui":y},renderOpts:{compile:function(){var a=c()(o()().mark(function d(){var r,u=arguments;return o()().wrap(function(e){for(;;)switch(e.prev=e.next){case 0:return e.next=2,n.e(92074).then(n.bind(n,92074));case 2:return e.abrupt("return",(r=e.sent).default.apply(r,u));case 3:case"end":return e.stop()}},d)}));function i(){return a.apply(this,arguments)}return i}()}},"card-demo-meta":{component:s.memo(s.lazy(function(){return n.e(91058).then(n.bind(n,86771))})),asset:{type:"BLOCK",id:"card-demo-meta",refAtomIds:["Card"],dependencies:{"index.tsx":{type:"FILE",value:n(40553).Z},"@yuntijs/ui":{type:"NPM",value:"1.2.0"}},entry:"index.tsx"},context:{"@yuntijs/ui":y},renderOpts:{compile:function(){var a=c()(o()().mark(function d(){var r,u=arguments;return o()().wrap(function(e){for(;;)switch(e.prev=e.next){case 0:return e.next=2,n.e(92074).then(n.bind(n,92074));case 2:return e.abrupt("return",(r=e.sent).default.apply(r,u));case 3:case"end":return e.stop()}},d)}));function i(){return a.apply(this,arguments)}return i}()}},"card-demo-grid":{component:s.memo(s.lazy(function(){return n.e(91058).then(n.bind(n,15583))})),asset:{type:"BLOCK",id:"card-demo-grid",refAtomIds:["Card"],dependencies:{"index.tsx":{type:"FILE",value:n(62143).Z},"@yuntijs/ui":{type:"NPM",value:"1.2.0"}},entry:"index.tsx"},context:{"@yuntijs/ui":y},renderOpts:{compile:function(){var a=c()(o()().mark(function d(){var r,u=arguments;return o()().wrap(function(e){for(;;)switch(e.prev=e.next){case 0:return e.next=2,n.e(92074).then(n.bind(n,92074));case 2:return e.abrupt("return",(r=e.sent).default.apply(r,u));case 3:case"end":return e.stop()}},d)}));function i(){return a.apply(this,arguments)}return i}()}},"card-demo-tab":{component:s.memo(s.lazy(function(){return n.e(91058).then(n.bind(n,99963))})),asset:{type:"BLOCK",id:"card-demo-tab",refAtomIds:["Card"],dependencies:{"index.tsx":{type:"FILE",value:n(10333).Z},"@yuntijs/ui":{type:"NPM",value:"1.2.0"},react:{type:"NPM",value:"18.3.1"}},entry:"index.tsx"},context:{"@yuntijs/ui":y,react:m||(m=n.t(s,2))},renderOpts:{compile:function(){var a=c()(o()().mark(function d(){var r,u=arguments;return o()().wrap(function(e){for(;;)switch(e.prev=e.next){case 0:return e.next=2,n.e(92074).then(n.bind(n,92074));case 2:return e.abrupt("return",(r=e.sent).default.apply(r,u));case 3:case"end":return e.stop()}},d)}));function i(){return a.apply(this,arguments)}return i}()}},"card-demo-playground":{component:s.memo(s.lazy(function(){return n.e(91058).then(n.bind(n,30407))})),asset:{type:"BLOCK",id:"card-demo-playground",refAtomIds:["Card"],dependencies:{"index.tsx":{type:"FILE",value:n(74944).Z},"@lobehub/ui":{type:"NPM",value:"1.164.2"},"@yuntijs/ui":{type:"NPM",value:"1.2.0"}},entry:"index.tsx"},context:{"@lobehub/ui":C,"@yuntijs/ui":y},renderOpts:{compile:function(){var a=c()(o()().mark(function d(){var r,u=arguments;return o()().wrap(function(e){for(;;)switch(e.prev=e.next){case 0:return e.next=2,n.e(92074).then(n.bind(n,92074));case 2:return e.abrupt("return",(r=e.sent).default.apply(r,u));case 3:case"end":return e.stop()}},d)}));function i(){return a.apply(this,arguments)}return i}()}}}},10212:function(l,t,n){"use strict";n.r(t),n.d(t,{texts:function(){return m}});const m=[{value:"based on antd ",paraId:0,tocIndex:0},{value:"Card",paraId:0,tocIndex:0},{value:" component.",paraId:0,tocIndex:0},{value:`import { Card } from '@yuntijs/ui';
+
+export default () => {
+  return (
+    <Card
+      title= 'YuntiUI Card'
+      bordered={true}
+      loading={false}
+      hoverable={true}
+    >
+    The YuntiUI components are inspired by LobeUI and developed based on Antd components, fully compatible with Antd components, and it is recommended to use antd-style as the default css-in-js styling solution.
+    </Children>
+  );
+};
+`,paraId:1,tocIndex:1}]},62143:function(l,t){"use strict";t.Z=`import { Card } from '@yuntijs/ui';
+
+export default () => {
+  const content =
+    'The YuntiUI components are inspired by LobeUI and developed based on Antd components, fully compatible with Antd components, and it is recommended to use antd-style as the default css-in-js styling solution. ';
+  const gridStyle: React.CSSProperties = {
+    width: '50%',
+    textAlign: 'center',
+  };
+  return (
+    <Card title="YuntiUI Card.Grid">
+      <Card.Grid hoverable={false} style={gridStyle}>
+        {content}
+      </Card.Grid>
+      <Card.Grid style={gridStyle}>{content}</Card.Grid>
+      <Card.Grid style={gridStyle}>{content}</Card.Grid>
+    </Card>
+  );
+};
+`},40553:function(l,t){"use strict";t.Z=`import { Card, Logo } from '@yuntijs/ui';
+
+export default () => {
+  const description =
+    'The YuntiUI components are inspired by LobeUI and developed based on Antd components, fully compatible with Antd components, and it is recommended to use antd-style as the default css-in-js styling solution. ';
+
+  return (
+    <Card>
+      <Card.Meta avatar={<Logo />} description={description} title="YuntiUI Card.Meta" />
+    </Card>
+  );
+};
+`},74944:function(l,t){"use strict";t.Z=`import { StoryBook, useControls, useCreateStore } from '@lobehub/ui';
+import type { CardProps } from '@yuntijs/ui';
+import { Card } from '@yuntijs/ui';
+
+export default () => {
+  const store = useCreateStore();
+  const control: CardProps | any = useControls(
+    {
+      title: 'YuntiUI Card',
+      extra: 'extra',
+      bordered: true,
+      loading: false,
+      hoverable: true,
+      children:
+        'The YuntiUI components are inspired by LobeUI and developed based on Antd components, fully compatible with Antd components, and it is recommended to use antd-style as the default css-in-js styling solution.',
+      type: {
+        options: ['inner'],
+        value: 'default',
+      },
+      size: {
+        options: ['default', 'small'],
+        value: 'default',
+      },
+    },
+    { store }
+  );
+  return (
+    <StoryBook levaStore={store}>
+      <Card {...control} />
+    </StoryBook>
+  );
+};
+`},10333:function(l,t){"use strict";t.Z=`import { Card } from '@yuntijs/ui';
+import { useState } from 'react';
+
+export default () => {
+  const tabList = [
+    {
+      key: 'Themeable',
+      label: 'Themeable',
+    },
+    {
+      key: 'Fast',
+      label: 'Fast',
+    },
+    {
+      key: 'Light & Dark UI',
+      label: 'Light & Dark UI',
+    },
+  ];
+  const [activeTabKey, setActiveTabKey] = useState<string>(tabList[0].key);
+  const onTabChange = (key: string) => {
+    setActiveTabKey(key);
+  };
+  const contentList: Record<string, React.ReactNode> = {
+    'Themeable': (
+      <p>
+        Provides a simple way to customize default themes, you can change the colors, fonts,
+        breakpoints and everything you need.
+      </p>
+    ),
+    'Fast': (
+      <p>
+        voids unnecessary styles props at runtime, making it more performant than other UI
+        libraries.
+      </p>
+    ),
+    'Light & Dark UI': (
+      <p>
+        Automatic dark mode recognition, NextUI automatically changes the theme when detects HTML
+        theme prop changes.
+      </p>
+    ),
+  };
+  return (
+    <Card
+      activeTabKey={activeTabKey}
+      onTabChange={onTabChange}
+      style={{ width: '100%' }}
+      tabBarExtraContent={<a href="#">More</a>}
+      tabList={tabList}
+      tabProps={{
+        size: 'middle',
+      }}
+    >
+      {contentList[activeTabKey]}
+    </Card>
+  );
+};
+`},28999:function(l,t){"use strict";t.Z=`import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
+import { Card } from '@yuntijs/ui';
+
+export default () => {
+  return (
+    <Card
+      actions={[
+        <SettingOutlined key="setting" />,
+        <EditOutlined key="edit" />,
+        <EllipsisOutlined key="ellipsis" />,
+      ]}
+      bordered={true}
+      cover={
+        <img
+          alt="example"
+          src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+        />
+      }
+      extra="extra"
+      hoverable={true}
+      loading={false}
+      title="YuntiUI Card"
+    >
+      The YuntiUI components are inspired by LobeUI and developed based on Antd components, fully
+      compatible with Antd components, and it is recommended to use antd-style as the default
+      css-in-js styling solution.
+    </Card>
+  );
+};
+`}}]);
---
+++ meta__Mentions.65f0e918.async.js
@@ -0,0 +1,336 @@
+(self.webpackChunk_yuntijs_ui=self.webpackChunk_yuntijs_ui||[]).push([[62474],{72239:function(c,t,n){var i={"./ar.json":[18444,18444],"./be.json":[5401,5401],"./cs.json":[24815,24815],"./de.json":[11976,11976],"./en.json":[28443,28443],"./es.json":[74957,74957],"./fa.json":[45263,45263],"./fi.json":[94234,94234],"./fr.json":[96792,96792],"./hi.json":[80489,80489],"./it.json":[59688,59688],"./ja.json":[98465,98465],"./ko.json":[7265,7265],"./nl.json":[67781,67781],"./pl.json":[29630,29630],"./pt.json":[15896,15896],"./ru.json":[59787,59787],"./sa.json":[84757,84757],"./tr.json":[22794,22794],"./uk.json":[54429,54429],"./vi.json":[97655,97655],"./zh.json":[50622,50622]};function p(r){if(!n.o(i,r))return Promise.resolve().then(function(){var o=new Error("Cannot find module '"+r+"'");throw o.code="MODULE_NOT_FOUND",o});var h=i[r],m=h[0];return n.e(h[1]).then(function(){return n.t(m,19)})}p.keys=function(){return Object.keys(i)},p.id=72239,c.exports=p},99082:function(c,t,n){"use strict";var i;n.r(t),n.d(t,{demos:function(){return b}});var p=n(90228),r=n.n(p),h=n(87999),m=n.n(h),o=n(75271),y=n(16911),f=n(594),v=n(48466),b={"mentions-demo-demos":{component:o.memo(o.lazy(function(){return n.e(23137).then(n.bind(n,62253))})),asset:{type:"BLOCK",id:"mentions-demo-demos",refAtomIds:["Mentions"],dependencies:{"index.tsx":{type:"FILE",value:n(15602).Z},"@lobehub/ui":{type:"NPM",value:"1.164.2"},"@yuntijs/ui":{type:"NPM",value:"1.2.0"},"lucide-react":{type:"NPM",value:"0.417.0"}},entry:"index.tsx"},context:{"@lobehub/ui":y,"@yuntijs/ui":f,"lucide-react":v},renderOpts:{compile:function(){var u=m()(r()().mark(function s(){var l,d=arguments;return r()().wrap(function(e){for(;;)switch(e.prev=e.next){case 0:return e.next=2,n.e(92074).then(n.bind(n,92074));case 2:return e.abrupt("return",(l=e.sent).default.apply(l,d));case 3:case"end":return e.stop()}},s)}));function a(){return u.apply(this,arguments)}return a}()}},"mentions-demo-chatinput":{component:o.memo(o.lazy(function(){return n.e(23137).then(n.bind(n,13969))})),asset:{type:"BLOCK",id:"mentions-demo-chatinput",refAtomIds:["Mentions"],dependencies:{"index.tsx":{type:"FILE",value:n(86141).Z},"@lobehub/ui":{type:"NPM",value:"1.164.2"},"@yuntijs/ui":{type:"NPM",value:"1.2.0"},"lucide-react":{type:"NPM",value:"0.417.0"},react:{type:"NPM",value:"18.3.1"}},entry:"index.tsx"},context:{"@lobehub/ui":y,"@yuntijs/ui":f,"lucide-react":v,react:i||(i=n.t(o,2))},renderOpts:{compile:function(){var u=m()(r()().mark(function s(){var l,d=arguments;return r()().wrap(function(e){for(;;)switch(e.prev=e.next){case 0:return e.next=2,n.e(92074).then(n.bind(n,92074));case 2:return e.abrupt("return",(l=e.sent).default.apply(l,d));case 3:case"end":return e.stop()}},s)}));function a(){return u.apply(this,arguments)}return a}()}},"mentions-demo-singleline":{component:o.memo(o.lazy(function(){return n.e(23137).then(n.bind(n,59937))})),asset:{type:"BLOCK",id:"mentions-demo-singleline",refAtomIds:["Mentions"],dependencies:{"index.tsx":{type:"FILE",value:n(66086).Z},"@lobehub/ui":{type:"NPM",value:"1.164.2"},"@yuntijs/ui":{type:"NPM",value:"1.2.0"},"lucide-react":{type:"NPM",value:"0.417.0"},react:{type:"NPM",value:"18.3.1"}},entry:"index.tsx"},context:{"@lobehub/ui":y,"@yuntijs/ui":f,"lucide-react":v,react:i||(i=n.t(o,2))},renderOpts:{compile:function(){var u=m()(r()().mark(function s(){var l,d=arguments;return r()().wrap(function(e){for(;;)switch(e.prev=e.next){case 0:return e.next=2,n.e(92074).then(n.bind(n,92074));case 2:return e.abrupt("return",(l=e.sent).default.apply(l,d));case 3:case"end":return e.stop()}},s)}));function a(){return u.apply(this,arguments)}return a}()}},"mentions-demo-json":{component:o.memo(o.lazy(function(){return n.e(23137).then(n.bind(n,10372))})),asset:{type:"BLOCK",id:"mentions-demo-json",refAtomIds:["Mentions"],dependencies:{"index.tsx":{type:"FILE",value:n(34421).Z},"@lobehub/ui":{type:"NPM",value:"1.164.2"},"@yuntijs/ui":{type:"NPM",value:"1.2.0"},"lucide-react":{type:"NPM",value:"0.417.0"}},entry:"index.tsx"},context:{"@lobehub/ui":y,"@yuntijs/ui":f,"lucide-react":v},renderOpts:{compile:function(){var u=m()(r()().mark(function s(){var l,d=arguments;return r()().wrap(function(e){for(;;)switch(e.prev=e.next){case 0:return e.next=2,n.e(92074).then(n.bind(n,92074));case 2:return e.abrupt("return",(l=e.sent).default.apply(l,d));case 3:case"end":return e.stop()}},s)}));function a(){return u.apply(this,arguments)}return a}()}},"mentions-demo-playground":{component:o.memo(o.lazy(function(){return n.e(23137).then(n.bind(n,71288))})),asset:{type:"BLOCK",id:"mentions-demo-playground",refAtomIds:["Mentions"],dependencies:{"index.tsx":{type:"FILE",value:n(45843).Z},"@lobehub/ui":{type:"NPM",value:"1.164.2"},"@yuntijs/ui":{type:"NPM",value:"1.2.0"},"lucide-react":{type:"NPM",value:"0.417.0"}},entry:"index.tsx"},context:{"@lobehub/ui":y,"@yuntijs/ui":f,"lucide-react":v},renderOpts:{compile:function(){var u=m()(r()().mark(function s(){var l,d=arguments;return r()().wrap(function(e){for(;;)switch(e.prev=e.next){case 0:return e.next=2,n.e(92074).then(n.bind(n,92074));case 2:return e.abrupt("return",(l=e.sent).default.apply(l,d));case 3:case"end":return e.stop()}},s)}));function a(){return u.apply(this,arguments)}return a}()}}}},93857:function(c,t,n){"use strict";n.r(t),n.d(t,{texts:function(){return i}});const i=[{value:"Based on ",paraId:0,tocIndex:0},{value:"lexical editor",paraId:0,tocIndex:0},{value:", inspired by ",paraId:0,tocIndex:0},{value:"Dify",paraId:0,tocIndex:0},{value:" and ",paraId:0,tocIndex:0},{value:"lexical-beautiful-mentions",paraId:0,tocIndex:0},{value:".",paraId:0,tocIndex:0},{value:`import { Icon } from '@lobehub/ui';
+import { Mentions } from '@yuntijs/ui';
+import { Smile } from 'lucide-react';
+
+export default () => {
+  return (
+    <Mentions
+      autoSize={{
+        minRows: 3,
+      }}
+      defaultValue="\u{1F44B}\uFF0CI'm {{1.zhang}}"
+      options={[{ label: 'zhang', value: '1.zhang', icon: <Icon icon={Smile} /> }]}
+      triggers={['@']}
+    />
+  );
+};
+`,paraId:1,tocIndex:1},{value:"Press shift + enter for line break, press enter to send and clear input.",paraId:2,tocIndex:2}]},86141:function(c,t){"use strict";t.Z=`/* eslint-disable no-console */
+import { Icon } from '@lobehub/ui';
+import { CLEAR_EDITOR_COMMAND, Divider, Flex, Mentions, MentionsEditor } from '@yuntijs/ui';
+import { Files, Smile } from 'lucide-react';
+import { useRef, useState } from 'react';
+
+export default () => {
+  const ref = useRef<MentionsEditor>(null);
+  const [value, setValue] = useState('');
+  const triggers = ['@', '#'];
+
+  return (
+    <Flex style={{ width: '100%' }} vertical>
+      <Mentions
+        autoSize={{
+          minRows: 3,
+          maxRows: 5,
+        }}
+        defaultValue="\u{1F44B}\uFF0CI'm {{1.zhang}}"
+        onPressEnter={value => {
+          setValue(value);
+          ref.current?.dispatchCommand(CLEAR_EDITOR_COMMAND, void 0);
+        }}
+        onTrigger={trigger => console.log('trigger =>', trigger)}
+        options={[
+          {
+            label: 'zhang',
+            value: '1.zhang',
+            icon: <Icon icon={Smile} />,
+          },
+          {
+            label: 'src',
+            value: 'src',
+            icon: <Icon icon={Files} />,
+            children: [
+              {
+                label: 'index.tsx',
+                value: 'index.tsx',
+              },
+              {
+                label: 'index.css',
+                selectedLabel: 'index.css',
+                value: 'index.css',
+              },
+            ],
+          },
+        ]}
+        preTriggerChars=".*"
+        ref={ref}
+        triggers={triggers}
+      />
+      <Divider />
+      <pre>{value}</pre>
+    </Flex>
+  );
+};
+`},34421:function(c,t){"use strict";t.Z=`import { Icon } from '@lobehub/ui';
+import { Mentions } from '@yuntijs/ui';
+import { Carrot, Cloud, Smile } from 'lucide-react';
+
+export default () => {
+  return (
+    <Mentions
+      autoSize={{
+        minRows: 5,
+      }}
+      code
+      defaultValue={\`{
+  "key1": {{2.luobo}},
+  \u201Ckey2": "{{1.zhang}}"
+}
+\`}
+      options={[
+        {
+          label: 'zhang',
+          value: '1.zhang',
+          icon: <Icon icon={Smile} />,
+          disabled: true,
+        },
+        {
+          label: 'luobo',
+          value: '2.luobo',
+          icon: <Icon icon={Carrot} />,
+          error: 'err',
+        },
+        {
+          label: 'yunti',
+          value: '3.yunti',
+          icon: <Icon icon={Cloud} />,
+        },
+      ]}
+      preTriggerChars=".*"
+      triggers={['@']}
+    />
+  );
+};
+`},45843:function(c,t){"use strict";t.Z=`import { Icon, StoryBook, useControls, useCreateStore } from '@lobehub/ui';
+import { Mentions, MentionsProps } from '@yuntijs/ui';
+import { Carrot, Cloud, Smile } from 'lucide-react';
+
+export default () => {
+  const store = useCreateStore();
+  const control: MentionsProps | any = useControls(
+    {
+      allowSpaces: true,
+      readOnly: false,
+      disabled: false,
+      placeholder: '\u8F93\u5165 @ \u89E6\u53D1\u63D0\u53CA',
+      variant: {
+        options: ['outlined', 'filled', 'borderless'],
+        value: 'outlined',
+      },
+      defaultValue: {
+        rows: true,
+        value: \`\u{1F44B}\uFF0CI'm {{1.zhang}}\`,
+      },
+      punctuation: {
+        rows: true,
+        value: '\\\\.,\\\\*\\\\?\\\\$\\\\|#{}\\\\(\\\\)\\\\^\\\\[\\\\]\\\\\\\\/!%\\'"~=<>_:;',
+      },
+      preTriggerChars: '.*',
+    },
+    { store }
+  );
+  return (
+    <StoryBook levaStore={store}>
+      <Mentions
+        autoSize={{ minRows: 6 }}
+        options={[
+          { label: 'zhang', value: '1.zhang', icon: <Icon icon={Smile} /> },
+          {
+            label: 'luobo',
+            value: '2.luobo',
+            icon: <Icon icon={Carrot} />,
+            error: '\u9009\u6211\u89E6\u53D1\u9519\u8BEF\u6837\u5F0F',
+          },
+          { label: 'yunti', value: '3.yunti', icon: <Icon icon={Cloud} /> },
+        ]}
+        style={{ width: 400 }}
+        {...control}
+      />
+    </StoryBook>
+  );
+};
+`},66086:function(c,t){"use strict";t.Z=`import { Icon } from '@lobehub/ui';
+import {
+  Button,
+  CLEAR_EDITOR_COMMAND,
+  Flex,
+  Mentions,
+  MentionsEditor,
+  textToEditorState,
+} from '@yuntijs/ui';
+import { Carrot, Cloud, Smile } from 'lucide-react';
+import { useRef } from 'react';
+
+export default () => {
+  const ref = useRef<MentionsEditor>(null);
+
+  return (
+    <Flex style={{ width: '100%' }} vertical>
+      <Mentions
+        autoSize={{
+          minRows: 1,
+        }}
+        defaultValue="\u{1F44B}\uFF0CI'm {{1.zhang}}"
+        options={[
+          {
+            label: 'zhang',
+            value: '1.zhang',
+            icon: <Icon icon={Smile} />,
+          },
+          {
+            label: 'luobo',
+            value: '2.luobo',
+            icon: <Icon icon={Carrot} />,
+            disabled: true,
+          },
+          {
+            label: 'yunti',
+            value: '3.yunti',
+            icon: <Icon icon={Cloud} />,
+          },
+        ]}
+        preTriggerChars=".*"
+        ref={ref}
+        triggers={['@']}
+      />
+      <Flex gap="small">
+        <Button
+          onClick={() => {
+            ref.current?.focus();
+          }}
+        >
+          focus
+        </Button>
+        <Button
+          onClick={() => {
+            ref.current?.update(textToEditorState("\u{1F44B}\uFF0CI'm {{3.yunti}}", ['@']));
+          }}
+        >
+          set
+        </Button>
+        <Button
+          onClick={() => {
+            ref.current?.dispatchCommand(CLEAR_EDITOR_COMMAND, void 0);
+          }}
+        >
+          clear
+        </Button>
+      </Flex>
+    </Flex>
+  );
+};
+`},15602:function(c,t){"use strict";t.Z=`/* eslint-disable no-console */
+import { Icon } from '@lobehub/ui';
+import { Mentions, Typography } from '@yuntijs/ui';
+import { Braces, Carrot, Cloud, Smile } from 'lucide-react';
+
+export default () => {
+  return (
+    <Mentions
+      autoSize={{
+        minRows: 3,
+        maxRows: 5,
+      }}
+      defaultValue="\u{1F44B}\uFF0CI'm {{1.zhang}}"
+      onTrigger={trigger => console.log('trigger =>', trigger)}
+      options={[
+        {
+          label: 'zhang',
+          value: '1.zhang',
+          icon: <Icon icon={Smile} />,
+        },
+        {
+          label: 'luobo',
+          value: '2.luobo',
+          icon: <Icon icon={Carrot} />,
+          error: '\u9009\u6211\u89E6\u53D1\u9519\u8BEF\u6837\u5F0F',
+        },
+        {
+          label: 'yunti',
+          value: '3.yunti',
+          icon: <Icon icon={Cloud} />,
+        },
+        {
+          label: 'json',
+          value: 'json',
+          icon: <Icon icon={Braces} />,
+          children: [
+            {
+              label: 'id',
+              selectedLabel: 'json.id',
+              value: 'json.id',
+              title: 'json.id (json \u7684 id \u5C5E\u6027)',
+            },
+            {
+              label: 'name',
+              selectedLabel: 'json.name',
+              value: 'json.name',
+            },
+            {
+              label: 'category',
+              value: 'json.category',
+              children: [
+                {
+                  label: 'id',
+                  selectedLabel: 'json.category.id',
+                  value: 'json.category.id',
+                  extraElement: <Typography.Text code>string</Typography.Text>,
+                },
+                {
+                  label: 'name',
+                  selectedLabel: 'json.category.name',
+                  value: 'json.category.name',
+                },
+              ],
+            },
+            {
+              label: 'photoUrls',
+              value: 'json.photoUrls',
+              children: [
+                {
+                  label: '[Array Item]',
+                  value: 'json.photoUrls.root',
+                },
+              ],
+            },
+            {
+              label: 'tags',
+              value: 'json.tags',
+              children: [
+                {
+                  label: '[Array Item]',
+                  value: 'json.tags.root',
+                  disabled: true,
+                },
+                {
+                  label: 'id',
+                  value: 'json.tags.id',
+                },
+                {
+                  label: 'name',
+                  value: 'json.tags.name',
+                },
+              ],
+            },
+            {
+              label: 'status',
+              value: 'json.status',
+            },
+          ],
+        },
+      ]}
+      preTriggerChars=".*"
+      triggers={['@', '#']}
+    />
+  );
+};
+`}}]);
